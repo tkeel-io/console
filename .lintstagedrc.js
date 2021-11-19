b0aaa2092bdf5806dcd0f040685f036dba0d7d84
Donnie An <acmilank22@gmail.com>
2021-11-19 12:31:27 +0800

    feat: init

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx,json,hbs,handlebars,css,scss,md}': 'prettier --write',
-  '*.{js,jsx,ts,tsx}': 'eslint --fix',
-  '*.{css,scss,js,jsx,ts,tsx}': 'stylelint --fix',
-  '**': 'cspell --no-must-find-files',
+  '*.{js,jsx,ts,tsx,json,hbs,handlebars,css,scss,md,yaml,yml}':
+    'prettier --write',
+  // '*.{js,jsx,ts,tsx}': 'eslint --fix',
+  // '*.{css,scss,js,jsx,ts,tsx}': 'stylelint --fix',
+  // '**': 'cspell --no-must-find-files',
 };
