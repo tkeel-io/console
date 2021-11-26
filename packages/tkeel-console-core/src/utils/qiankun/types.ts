589ef59f2847e7fd0a7169fb14182fbeec0f1705
jackli <jackli@yunify.com>
2021-11-26 15:11:02 +0800

    chore: update package.json and tsconfig.json

--- packages/tkeel-console-core/src/utils/qiankun/types.ts
+++ packages/tkeel-console-core/src/utils/qiankun/types.ts
@@ -11,5 +11,6 @@ export interface IApp {
     | IActiveRuleFunction
     | Array<string | IActiveRuleFunction>;
   loader?: (loading: boolean) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   props?: Record<any, any>;
 }
