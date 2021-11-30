392c53c581821bb809b38c838a5d27d15ea04ce4
donniean <acmilank22@gmail.com>
2021-11-30 14:33:59 +0800

    init (#8)
    * feat: init
    
    * feat: use monorepos
    
    * feat: init
    
    * chore: modified gitignore
    
    * chore: modified ignore files
    
    * chore: prettier
    
    * chore: add yarn plugins
    
    * chore: add tsconfig-paths-webpack-plugin
    
    * chore: change package.json
    
    * chore: upgrade dependencies
    
    * chore: add tsconfig
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m

--- packages/tkeel-console-core/src/mock/index.ts
+++ packages/tkeel-console-core/src/mock/index.ts
@@ -2,22 +2,10 @@ import { IMenu } from './types';
 
 const MENUS: IMenu[] = [
   {
-    id: 'app-01',
-    name: 'react-app',
-    path: '/react-app',
+    id: 'plugin-example',
+    name: 'plugin-example',
+    path: '/plugin-example',
     entry: 'http://127.0.0.1:3001',
-  },
-  {
-    id: 'app-02',
-    name: 'vue-app',
-    path: '/vue-app',
-    entry: 'http://127.0.0.1:3002',
-  },
-  {
-    id: 'app-03',
-    name: 'html-app',
-    path: '/html-app',
-    entry: 'http://127.0.0.1:3003',
   },
 ];
 
