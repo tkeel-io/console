80777fe9c0db0d57b47687bd2ce0cea2492d96b6
donniean <acmilank22@gmail.com>
2021-12-01 17:16:23 +0800

    build: upgrade yarn

--- packages/tkeel-console-core/src/App.tsx
+++ packages/tkeel-console-core/src/App.tsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from 'react';
 // import { BrowserRouter as Router } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-import { useRequest } from '@tkeel/console-hooks';
 import { initGlobalState, MicroAppStateActions } from 'qiankun';
 
 import Layout from '@/containers/Layout';
@@ -26,11 +25,6 @@ function App() {
       console.error(error);
     }
   };
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const r = useRequest({ url: 'https://api.github.com/repositories?q=react' });
-  // eslint-disable-next-line no-console
-  console.log(r);
 
   const init = async () => {
     await fetchData();
