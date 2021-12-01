9d62568a6a2c5cb1481b8b330094571c5c5cb3b5
jackli <jackli@yunify.com>
2021-12-01 16:45:13 +0800

    feat: plugin-example add @types

--- packages/tkeel-console-core/src/containers/Layout/index.tsx
+++ packages/tkeel-console-core/src/containers/Layout/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from '@/containers/Layout/Header';
 import Menus from '@/containers/Layout/Menus';
@@ -16,13 +15,11 @@ type Props = {
 function Layout({ children, menus }: Props): JSX.Element {
   return (
     <LayoutWrapper>
-      <Router>
-        <Menus data={menus} />
-        <LayoutContent>
-          <Header />
-          {children}
-        </LayoutContent>
-      </Router>
+      <Menus data={menus} />
+      <LayoutContent>
+        <Header />
+        {children}
+      </LayoutContent>
     </LayoutWrapper>
   );
 }
