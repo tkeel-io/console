0f4e2df5013f3d15bbac374253acae294a6b8f0b
jackli <jackli@yunify.com>
2021-12-03 11:33:11 +0800

    chore: remove unused code

--- packages/tkeel-console-core/src/routes/index.tsx
+++ packages/tkeel-console-core/src/routes/index.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import { Navigate, Route, Routes as ReactRouterRoutes } from 'react-router-dom';
 
-// import { Navigate, useRoutes } from 'react-router-dom';
 import PageNotFound from '@/components/PageNotFound';
 import { IApp } from '@/utils/qiankun/types';
 
 type Props = {
   data: IApp[];
 };
-
-// function Routes({ data }: Props) {
-//   const dataConfig = data.map(({ container, activeRule }) => ({
-//     path: `${activeRule}/*`,
-//     element: <div id={container.replace(/^#/, '')} />,
-//   }));
-
-//   const config = [
-//     {
-//       path: '/',
-//       element: <Navigate replace to={data[0].activeRule} />,
-//     },
-//     ...dataConfig,
-//     {
-//       path: '*',
-//       element: <PageNotFound />,
-//     },
-//   ];
-
-//   return useRoutes(config);
-// }
 
 function Routes({ data }: Props) {
   return (
