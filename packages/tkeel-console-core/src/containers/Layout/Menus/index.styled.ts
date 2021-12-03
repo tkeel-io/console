a9008be0275192ef85a565ee0c1401de9dc96f2b
jackzerone <74034572+jackzerone@users.noreply.github.com>
2021-12-03 11:36:18 +0800

    add theme folder (#22)
    * chore: update package.json and tsconfig.json
    
    * feat: init Layout
    
    * feat: plugin-example add @types
    
    * feat: modify routes
    
    * feat: add theme
    
    * feat: add theme
    
    * chore: remove unused code

--- packages/tkeel-console-core/src/containers/Layout/Menus/index.styled.ts
+++ packages/tkeel-console-core/src/containers/Layout/Menus/index.styled.ts
@@ -1,3 +1,4 @@
+import { Link } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 export const LayoutMenus = styled.div`
@@ -12,36 +13,33 @@ export const TitleWrapper = styled.div`
   height: 92px;
   padding-left: 24px;
   border-bottom: 1px solid #e2e8f0;
-
-  img {
-    width: 27px;
-  }
 `;
 
 export const Title = styled.h1`
   margin-left: 10px;
+  color: #2d3748;
   font-weight: bold;
-  font-size: 48px;
   font-size: 18px;
 `;
 
 export const List = styled.div`
   padding: 24px;
+`;
 
-  a {
-    display: block;
-    display: flex;
-    align-items: center;
-    height: 48px;
-    margin-bottom: 4px;
-    padding-left: 22px;
+export const Item = styled(Link)`
+  display: flex;
+  align-items: center;
+  height: 48px;
+  margin-bottom: 4px;
+  padding-left: 22px;
+  color: #718096;
+  font-weight: 500;
 
-    &.active {
-      color: #fff;
-      background-color: #2d3748;
-      border-radius: 4px;
-      box-shadow: 0 20px 25px -5px rgb(113 128 150 / 10%),
-        0 10px 10px -5px rgb(113 128 150 / 4%);
-    }
+  &.active {
+    color: #fff;
+    background-color: #2d3748;
+    border-radius: 4px;
+    box-shadow: 0 20px 25px -5px rgb(113 128 150 / 10%),
+      0 10px 10px -5px rgb(113 128 150 / 4%);
   }
 `;
