3a8840d56e61ee6e13a43da29a70d66037de474f
jackli <jackli@yunify.com>
2021-12-10 11:54:53 +0800

    chore: remove plugin-manage

--- packages/tkeel-console-plugin-manage/src/public-path.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
-// @ts-ignore
-if (window.__POWERED_BY_QIANKUN__) {
-  // @ts-ignore
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-}
