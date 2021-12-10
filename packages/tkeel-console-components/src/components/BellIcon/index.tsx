c68d06b8f0505e2b53487378439c7d5d98442946
donniean <acmilank22@gmail.com>
2021-12-10 18:05:25 +0800

    add icons (#48)
    * feat: init
    
    * feat: use monorepos
    
    * feat: init
    
    * chore: modified gitignore
    
    * chore: modified ignore files
    
    * chore: prettier
    
    * chore: add yarn plugins
    
    * chore: add tsconfig-paths-webpack-plugin
    
    * chore: change package.json
    
    * chore: upgrade dependencies
    
    * chore: add tsconfig
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * build: upgrade dependencies
    
    * feat: add tkeel-console-components
    
    * feat: add tkeel-console-components
    
    * feat: add tkeel-console-utils and tkeel-console-hooks
    
    * feat: add tkeel-console-utils and tkeel-console-hooks
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: node-config => dotenv
    
    * build: node-config => dotenv
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: uninstall stylelint-config-styled-components
    
    * build: uninstall stylelint-config-styled-components
    
    * feat: BASENAME => BASE_PATH
    
    * feat: BASENAME => BASE_PATH
    
    * feat: add create plugin script
    
    * feat: add create plugin script
    
    * feat: add create plugin script
    
    * feat: add check-packages script
    
    * feat: modified themes
    
    * feat: modified themes
    
    * feat: modified themes
    
    * feat: modified favicon
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: add  scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * docs: modified README
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: add svgr
    
    * feat: modified d.ts
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: lint
    
    * chore: uninstall shelljs
    
    * feat: m
    
    * chore: uninstall shelljs
    
    * chore: uninstall shelljs
    
    * add: @types/tkeel-console
    
    * add: @types/tkeel-console
    
    * add: @types/tkeel-console
    
    * feat: add icons

---
+++ packages/tkeel-console-components/src/components/BellIcon/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Bell from '../../assets/icons/bell.svg?svgr';
+import Icon from '../Icon';
+
+function BellIcon() {
+  return <Icon svgComponent={Bell} />;
+}
+
+export default BellIcon;
