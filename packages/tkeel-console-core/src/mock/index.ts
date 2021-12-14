7a7b5fd33900763838e86aa86b392829a6ea02ea
donniean <acmilank22@gmail.com>
2021-12-14 12:19:07 +0800

    add icons (#50)
    * feat: init
    
    * feat: use monorepos
    
    * feat: init
    
    * chore: modified gitignore
    
    * chore: modified ignore files
    
    * chore: prettier
    
    * chore: add yarn plugins
    
    * chore: add tsconfig-paths-webpack-plugin
    
    * chore: change package.json
    
    * chore: upgrade dependencies
    
    * chore: add tsconfig
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: init
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * build: upgrade dependencies
    
    * feat: add tkeel-console-components
    
    * feat: add tkeel-console-components
    
    * feat: add tkeel-console-utils and tkeel-console-hooks
    
    * feat: add tkeel-console-utils and tkeel-console-hooks
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: upgrade yarn
    
    * build: node-config => dotenv
    
    * build: node-config => dotenv
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: move env.js to scripts/utils/
    
    * build: uninstall stylelint-config-styled-components
    
    * build: uninstall stylelint-config-styled-components
    
    * feat: BASENAME => BASE_PATH
    
    * feat: BASENAME => BASE_PATH
    
    * feat: add create plugin script
    
    * feat: add create plugin script
    
    * feat: add create plugin script
    
    * feat: add check-packages script
    
    * feat: modified themes
    
    * feat: modified themes
    
    * feat: modified themes
    
    * feat: modified favicon
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: add  scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * feat: dev scripts
    
    * docs: modified README
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: rename
    
    * feat: add svgr
    
    * feat: modified d.ts
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: add svgr
    
    * feat: lint
    
    * chore: uninstall shelljs
    
    * feat: m
    
    * chore: uninstall shelljs
    
    * chore: uninstall shelljs
    
    * add: @types/tkeel-console
    
    * add: @types/tkeel-console
    
    * add: @types/tkeel-console
    
    * feat: add icons
    
    * feat: add icons
    
    * @tkeel/console-components
    
    * feat: add icons

--- packages/tkeel-console-core/src/mock/index.ts
+++ packages/tkeel-console-core/src/mock/index.ts
@@ -13,51 +13,18 @@ const MENUS: IMenu[] = [
       },
     ],
   },
-  // {
-  //   categoryId: 'default',
-  //   menus: [
-  //     {
-  //       id: 'plugin-plugins',
-  //       name: 'plugins',
-  //       icon: 'summary',
-  //       path: '/plugins',
-  //       entry: 'http://127.0.0.1:3002',
-  //     },
-  //   ],
-  // },
-  // {
-  //   categoryId: 'dataOrganization',
-  //   categoryName: '数据组织',
-  //   menus: [
-  //     {
-  //       id: 'plugin-vue-example',
-  //       name: '设备管理',
-  //       icon: 'house',
-  //       path: '/plugin-vue-example',
-  //       entry: 'http://127.0.0.1:3002',
-  //     },
-  //   ],
-  // },
-  // {
-  //   categoryId: 'dataOrganization',
-  //   categoryName: '数据组织',
-  //   menus: [
-  //     {
-  //       id: 'device-manage',
-  //       name: '设备管理',
-  //       icon: 'house',
-  //       children: [
-  //         {
-  //           id: 'plugin-vue-example',
-  //           name: '添加设备',
-  //           icon: 'house',
-  //           path: '/plugin-vue-example',
-  //           entry: 'http://127.0.0.1:3002',
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
+  {
+    categoryId: 'default',
+    menus: [
+      {
+        id: 'plugin-plugins',
+        name: 'plugins',
+        icon: 'summary',
+        path: '/plugins',
+        entry: 'http://127.0.0.1:3002',
+      },
+    ],
+  },
 ];
 
 function fetchMenus(): Promise<IMenu[]> {
--- packages/tkeel-console-core/src/pages/PageNotFound/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Box } from '@tkeel/console-components';
-
-function PageNotFound() {
-  return <Box>PageNotFound</Box>;
-}
-
-export default PageNotFound;
