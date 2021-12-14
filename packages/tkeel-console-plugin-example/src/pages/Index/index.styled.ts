dd58623cd88db585e1d6595e042f0218ad0d053f
donniean <acmilank22@gmail.com>
2021-12-14 16:17:35 +0800

    feat: remove example

--- packages/tkeel-console-plugin-example/src/pages/Index/index.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled';
-import { Container, Heading } from '@tkeel/console-components';
-
-export const Wrapper = styled(Container)``;
-
-export const Title = styled(Heading)``;
