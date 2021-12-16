c232772d27aedce8f9631ecb70457b473cfb91dc
donniean <acmilank22@gmail.com>
2021-12-16 17:15:42 +0800

    feat: modified export

--- packages/tkeel-console-components/src/index.ts
+++ packages/tkeel-console-components/src/index.ts
@@ -1,5 +1,3 @@
-// export * from './components/Form';
-
 export { default as Form } from './components/Form';
 export { default as PageHeader } from './components/PageHeader';
 export { default as SearchInput } from './components/SearchInput';
