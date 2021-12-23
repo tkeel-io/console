6adbc99a44eeb4bf6737a4e5b104ccdbca1cc1d9
jackli <jackli@yunify.com>
2021-12-23 18:10:12 +0800

    feat: modify plugin detail

--- packages/tkeel-console-plugin-plugins/src/pages/Detail/index.tsx
+++ packages/tkeel-console-plugin-plugins/src/pages/Detail/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 // import { useParams } from 'react-router-dom';
 import { Box, Button, Flex } from '@chakra-ui/react';
+import { ChevronLeftFilledIcon } from '@tkeel/console-icons';
 
 import InstallButton from '@/components/InstallButton';
 
@@ -9,23 +11,30 @@ function Detail() {
   // eslint-disable-next-line no-console
   // console.log('Detail ~ params.id', params.id);
 
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate('/');
+  };
   return (
     <Flex padding="20px" justifyContent="space-between">
       <Box width="360px">
         <Box height="124px" padding="16px" backgroundColor="gray.50">
           <Flex height="28px" justifyContent="space-between">
-            <Button variant="outline" size="sm">
+            <Button
+              variant="outline"
+              size="sm"
+              leftIcon={<ChevronLeftFilledIcon />}
+              onClick={goBack}
+            >
               返回
             </Button>
             <InstallButton size="sm" />
           </Flex>
-          <Flex marginTop="16px">device</Flex>
+          <Flex marginTop="16px" />
         </Box>
-        basic Info
       </Box>
-      <Box marginLeft="20px" flex="1">
-        detail22
-      </Box>
+      <Box marginLeft="20px" flex="1" />
     </Flex>
   );
 }
