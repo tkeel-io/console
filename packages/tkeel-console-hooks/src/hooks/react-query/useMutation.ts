e81129b9ed8a2ac0112bf6c1810d2421eae5827a
donniean <acmilank22@gmail.com>
2021-12-28 16:32:04 +0800

    feat: useMutation

--- packages/tkeel-console-hooks/src/hooks/react-query/useMutation.ts
+++ packages/tkeel-console-hooks/src/hooks/react-query/useMutation.ts
@@ -1,44 +1,18 @@
 import { useMutation } from 'react-query';
-import {
-  AxiosRequestConfigExtended,
-  AxiosResponseExtended,
-  request,
-  RequestResult,
-} from '@tkeel/console-utils';
-import { get, merge } from 'lodash';
+import { RequestResult } from '@tkeel/console-utils';
 
 import { UseCustomMutationOptions } from './types';
+import { getUseMutationOptions, transformResult } from './utils';
 
-export default function useCustomMutation<T>(
-  options: UseCustomMutationOptions
+export default function useCustomMutation<T, D>(
+  options: UseCustomMutationOptions<T, D>
 ) {
-  const {
-    url,
-    method,
-    params,
-    data,
-    extras,
-    axiosRequestConfig,
-    reactQueryOptions,
-  } = options;
-  // const key = reactQueryOptions?.mutationKey;
-  // const mutationKey = key || [omitBy({ url, method, params, data }, isNil)];
-  const config = merge(
-    {},
-    { url, method, params, data, extras },
-    axiosRequestConfig
-  );
-
-  function mutationFn(
-    variables: AxiosRequestConfigExtended
-  ): Promise<RequestResult<T>> {
-    return request(merge({}, config, variables));
-  }
-
-  const opts = merge({}, { mutationFn }, reactQueryOptions);
-  const { data: ret, ...rest } = useMutation<unknown, unknown, any>(opts);
-  const d: T | unknown = get(ret, 'data');
-  const response: AxiosResponseExtended<T> | unknown = get(ret, 'response');
-
-  return { data: d, response, ...rest };
+  const opts = getUseMutationOptions<T, D>(options);
+  const { mutationKey } = opts;
+  const result = useMutation<RequestResult<T>, unknown, unknown>(opts);
+  return transformResult<T>({
+    keyName: 'mutationKey',
+    key: mutationKey,
+    result,
+  });
 }
