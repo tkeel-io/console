2d0c816867ba859ffebeae5797165ebad1293c2e
donniean <acmilank22@gmail.com>
2022-01-03 18:38:04 +0800

    feat: m

--- packages/tkeel-console-themes/src/themes/kubesphere-light/index.ts
+++ packages/tkeel-console-themes/src/themes/kubesphere-light/index.ts
@@ -1,10 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
+import baseExtension from '@/tkeel-console-themes/themes//base-extension';
+
 import components from './components';
 import foundations from './foundations';
 import styles from './styles';
-
-import baseExtension from '@/tkeel-console-themes/themes//base-extension';
 
 const theme = extendTheme(baseExtension, {
   config: { initialColorMode: 'light' },
