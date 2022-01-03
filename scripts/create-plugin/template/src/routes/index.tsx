aecdf636c2014147eccd11a74ef42a9d593b6f01
donniean <acmilank22@gmail.com>
2022-01-03 18:45:31 +0800

    feat: m

---
+++ scripts/create-plugin/template/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes as ReactRouterRoutes } from 'react-router-dom';
+
+import Index from '../pages/Index';
+
+function Routes() {
+  return (
+    <ReactRouterRoutes>
+      <Route path="/" element={<Index />} />
+    </ReactRouterRoutes>
+  );
+}
+
+export default Routes;
