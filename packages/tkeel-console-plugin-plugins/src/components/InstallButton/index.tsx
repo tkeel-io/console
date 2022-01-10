3360d8f01b10196fe368ac2a218ec84eb554c114
jackli <jackli@yunify.com>
2022-01-10 16:35:32 +0800

    chore: modify install button style

--- packages/tkeel-console-plugin-plugins/src/components/InstallButton/index.tsx
+++ packages/tkeel-console-plugin-plugins/src/components/InstallButton/index.tsx
@@ -1,6 +1,9 @@
-import React, { MouseEventHandler, useState } from 'react';
-import { Button } from '@chakra-ui/react';
-import { DownloadFilledIcon, LoadingFilledIcon } from '@tkeel/console-icons';
+import React, { MouseEventHandler } from 'react';
+import { Button, Text, useDisclosure } from '@chakra-ui/react';
+import { Editor, Modal } from '@tkeel/console-components';
+import { DownloadFilledIcon } from '@tkeel/console-icons';
+
+import mockParams from '@/tkeel-console-plugin-plugins/pages/Detail/mockParams';
 
 type Props = {
   size?: string;
@@ -11,37 +14,64 @@ const defaultProps = {
 };
 
 function InstallButton({ size }: Props) {
-  const [loading, setLoading] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   let timer: number | null = null;
 
   const handleInstall: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
     if (!timer) {
-      setLoading(true);
-      setInterval(() => {});
       timer = window.setTimeout(() => {
-        setLoading(false);
         timer = null;
+        onClose();
       }, 500);
     }
   };
 
   return (
-    <Button
-      colorScheme="primary"
-      variant="solid-no-shadow"
-      size={size}
-      leftIcon={
-        loading ? (
-          <LoadingFilledIcon color="white" size={12} />
-        ) : (
-          <DownloadFilledIcon size={12} />
-        )
-      }
-      onClick={handleInstall}
-    >
-      安装
-    </Button>
+    <>
+      <Button
+        colorScheme="primary"
+        variant="solid-no-shadow"
+        size={size}
+        leftIcon={<DownloadFilledIcon size={12} />}
+        onClick={(e) => {
+          e.stopPropagation();
+          onOpen();
+        }}
+      >
+        安装
+      </Button>
+      <Modal
+        title={
+          <Text color="gray.800" fontSize="14px">
+            设置配置
+          </Text>
+        }
+        footer={
+          <>
+            <Button onClick={onClose}>取消</Button>
+            <Button
+              marginLeft="12px"
+              colorScheme="primary"
+              onClick={handleInstall}
+            >
+              确定
+            </Button>
+          </>
+        }
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <Editor
+          width="100%"
+          height="416px"
+          language="yaml"
+          value={mockParams}
+          readOnly
+        />
+      </Modal>
+    </>
   );
 }
 
