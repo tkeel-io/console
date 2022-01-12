a2b309b303cac0d5c980c3dea9525331f95c4beb
donniean <acmilank22@gmail.com>
2022-01-12 16:17:21 +0800

    feat: modified import

--- packages/tkeel-console-portal-admin/src/index.tsx
+++ packages/tkeel-console-portal-admin/src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import App from '@tkeel/console-portal-base';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
