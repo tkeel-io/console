4defa11d1928bf992924443020e56a72d2ea39db
donniean <acmilank22@gmail.com>
2022-02-11 18:52:47 +0800

    Revert "Feat/devices (#174)" (#175)
    This reverts commit 781cda1f8e89b4b645e46355802db1153b430cfe.

--- packages/tkeel-console-plugin-tenant-devices/src/components/modals/CreateDeviceModal/BasicInfoPart.tsx
+++ packages/tkeel-console-plugin-tenant-devices/src/components/modals/CreateDeviceModal/BasicInfoPart.tsx
@@ -1,38 +1,27 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { UseFormReturn } from 'react-hook-form';
-import { Stack, Text } from '@chakra-ui/react';
-import {
-  Checkbox,
-  CheckboxGroup,
-  FormControl,
-  FormField,
-} from '@tkeel/console-components';
-import { map } from 'lodash';
+import { Checkbox, Text } from '@chakra-ui/react';
+import { FormControl, FormField } from '@tkeel/console-components';
 
-import { ConnectInfoType, DeviceValueType } from './types';
+import { DeviceValueType } from './types';
 
 const { TextField, SelectField, TextareaField } = FormField;
 
-const CONNECT_OPTION = {
-  DIRECT: '直连',
-  INDIRECT: '非直连',
-};
-
 interface Props {
   formHandler: UseFormReturn<DeviceValueType, object>;
-  watchFields: DeviceValueType;
 }
-export default function BasicInfoPart({ formHandler, watchFields }: Props) {
-  const { register, formState, setValue } = formHandler;
+export default function BasicInfoPart({ formHandler }: Props) {
+  const { register, watch, formState } = formHandler;
   const { errors } = formState;
+  const watchFields = watch();
   // eslint-disable-next-line no-console
-  console.log(watchFields);
+  console.log('watchFields', watchFields);
   return (
     <>
       <TextField
         id="name"
         label="设备组名称"
-        registerReturn={register('name', {
+        schemas={register('name', {
           required: { value: true, message: 'required' },
         })}
         error={errors.name}
@@ -41,7 +30,7 @@ export default function BasicInfoPart({ formHandler, watchFields }: Props) {
         id="parent"
         label="父设备组"
         options={[{ value: 1, label: '默认设备组' }]}
-        registerReturn={register('parent', {
+        schemas={register('parent', {
           required: { value: true, message: 'required' },
         })}
         error={errors.parent}
@@ -49,56 +38,39 @@ export default function BasicInfoPart({ formHandler, watchFields }: Props) {
       <SelectField
         label="设备连接方式"
         id="directConnection"
-        options={map(CONNECT_OPTION, (value) => {
-          return { label: value, value };
-        })}
-        registerReturn={register('directConnection', {
+        options={[
+          { value: 1, label: '直连' },
+          { value: 0, label: '非直连' },
+        ]}
+        schemas={register('directConnection', {
           required: { value: true, message: 'required' },
         })}
         error={errors.directConnection}
       />
-      <FormControl id="connectOption">
-        <CheckboxGroup
-          onChange={(value: ConnectInfoType[]) => {
-            setValue('connectOption', value);
-          }}
-          value={
-            watchFields.directConnection === '非直连'
-              ? [ConnectInfoType.useTemplate]
-              : watchFields.connectOption
-          }
-        >
-          <Stack spacing="16px" direction="column">
-            <Checkbox
-              colorScheme="primary"
-              id="useTemplate"
-              value={ConnectInfoType.useTemplate}
-              isDisabled={watchFields.directConnection === '非直连'}
-            >
-              <Text color="gray.600" fontSize="14px">
-                使用设备模版
-              </Text>
-            </Checkbox>
-            <Checkbox
-              colorScheme="primary"
-              id="selfLearn"
-              value={ConnectInfoType.selfLearn}
-              isDisabled={watchFields.directConnection === '非直连'}
-            >
-              <Text color="gray.600" fontSize="14px">
-                自学习模式
-              </Text>
-            </Checkbox>
-          </Stack>
-        </CheckboxGroup>
+      <FormControl id="useTemplate">
+        <Checkbox colorScheme="primary" schemas={register('useTemplate')}>
+          <Text color="gray.600" fontSize="14px">
+            使用设备模版
+          </Text>
+        </Checkbox>
       </FormControl>
+      <FormControl id="selfLearn">
+        <Checkbox
+          colorScheme="primary"
+          color="gray.600"
+          schemas={register('selfLearn', {})}
+        >
+          <Text color="gray.600" fontSize="14px">
+            自学习模式
+          </Text>
+        </Checkbox>
+      </FormControl>
+
       <TextareaField
         id="desc"
         label="描述"
         placeholder="请输入"
-        type="text"
-        registerReturn={register('desc')}
-        error={errors.desc}
+        schemas={register('desc', {})}
       />
     </>
   );
