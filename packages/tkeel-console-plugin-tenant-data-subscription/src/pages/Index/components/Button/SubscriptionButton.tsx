bf29ce4bb48be9de36d68f7559abc3a4315f9458
zhanghan <51943297+ZhangHOOO@users.noreply.github.com>
2022-02-14 14:42:53 +0800

    Merge branch 'main' of git://github.com/tkeel-io/console into main (#180)
    * feat: init tenant-data-subscription
    
    * feat: add data-subscription
    
    * feat: init data-subscription-detail
    
    Co-authored-by: donniean <acmilank22@gmail.com>

---
+++ packages/tkeel-console-plugin-tenant-data-subscription/src/pages/Index/components/Button/SubscriptionButton.tsx
@@ -0,0 +1,15 @@
+import { ButtonProps } from '@chakra-ui/react';
+import { AddFilledIcon } from '@tkeel/console-icons';
+
+import IconButton from './IconButton';
+
+function CreateButton({ children, ...rest }: ButtonProps) {
+  return (
+    // @ts-ignore
+    <IconButton icon={<AddFilledIcon color="white" />} {...rest}>
+      {children}
+    </IconButton>
+  );
+}
+
+export default CreateButton;
