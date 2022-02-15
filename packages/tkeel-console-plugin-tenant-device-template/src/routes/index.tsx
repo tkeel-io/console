c571a76e140e8273dff549c97973e090c3d9bc0d
fangshun@ <71168119+fangshun-z@users.noreply.github.com>
2022-02-15 16:24:27 +0800

    commit devices plugin (#189)
    * feat: create device-template plugin
    
    * feat: add buttons and modals
    
    * feat: modify config
    
    * feat: add Tabs
    
    * feat: modify some components
    
    Co-authored-by: FangShun <davidfang@yunify.com>
    Co-authored-by: donniean <acmilank22@gmail.com>

--- packages/tkeel-console-plugin-tenant-device-template/src/routes/index.tsx
+++ packages/tkeel-console-plugin-tenant-device-template/src/routes/index.tsx
@@ -2,13 +2,10 @@ import { Route, Routes as ReactRouterRoutes } from 'react-router-dom';
 
 import Index from '../pages/Index';
 
-import DeviceDetail from '@/tkeel-console-plugin-tenant-devices/pages/DeviceDetail';
-
 function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<Index />} />
-      <Route path="/detail" element={<DeviceDetail />} />
     </ReactRouterRoutes>
   );
 }
