2f6a3cbc4ad7d6c9c8af6f350a40a6b12260ada0
donniean <acmilank22@gmail.com>
2022-02-17 20:07:26 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-tenants/src/pages/Index/components/CreateTenantButton/index.tsx
+++ packages/tkeel-console-plugin-admin-tenants/src/pages/Index/components/CreateTenantButton/index.tsx
@@ -1,15 +1,23 @@
 import { useDisclosure } from '@chakra-ui/react';
 import { CreateButton } from '@tkeel/console-components';
 
-import EditSpaceModal from '@/tkeel-console-plugin-admin-tenants/pages/Index/components/EditSpaceModal';
+import CreateTenantModal from '@/tkeel-console-plugin-admin-tenants/pages/Index/components/CreateTenantModal';
 
 export default function CreateTenantButton() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
-      <CreateButton onClick={onOpen}>创建用户</CreateButton>
-      {isOpen && <EditSpaceModal isOpen={isOpen} onClose={onClose} />}
+      <CreateButton onClick={onOpen}>创建</CreateButton>
+      {isOpen && (
+        <CreateTenantModal
+          isOpen={isOpen}
+          isConfirmButtonLoading={false}
+          formFields={{}}
+          onClose={onClose}
+          onConfirm={() => {}}
+        />
+      )}
     </>
   );
 }
