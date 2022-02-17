e52c2ff5969b6540392368c80081a408f71d11ac
donniean <acmilank22@gmail.com>
2022-02-17 20:11:06 +0800

    feat: m

---
+++ packages/tkeel-console-plugin-admin-tenants/src/pages/Index/components/ModifyTenantButton/index.tsx
@@ -0,0 +1,33 @@
+import { useDisclosure } from '@chakra-ui/react';
+import { LinkButton } from '@tkeel/console-components';
+
+import { Tenant } from '@/tkeel-console-plugin-admin-tenants/hooks/queries/useTenantsQuery';
+import ModifyTenantModal from '@/tkeel-console-plugin-admin-tenants/pages/Index/components/ModifyTenantModal';
+
+interface Props {
+  data: Tenant;
+  onSuccess: () => void;
+}
+
+export default function ModifyTenantButton({ data, onSuccess }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  // eslint-disable-next-line no-console
+  console.log(data, onSuccess);
+
+  return (
+    <>
+      <LinkButton onClick={onOpen}>修改</LinkButton>
+      {isOpen && (
+        <ModifyTenantModal
+          isOpen={isOpen}
+          isConfirmButtonLoading={false}
+          formFields={{}}
+          defaultValues={{ title: '', admin: '' }}
+          onClose={onClose}
+          onConfirm={() => {}}
+        />
+      )}
+    </>
+  );
+}
