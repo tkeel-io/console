0af979f069521ba90e1733a69499ac76961eae56
donniean <acmilank22@gmail.com>
2022-02-18 18:03:45 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-tenants/src/pages/Index/components/ModifyTenantModal/index.tsx
+++ packages/tkeel-console-plugin-admin-tenants/src/pages/Index/components/ModifyTenantModal/index.tsx
@@ -1,34 +1,71 @@
-import BaseTenantModal, {
-  FormFields,
-  FormValues,
-} from '@/tkeel-console-plugin-admin-tenants/components/BaseTenantModal';
+import { useForm } from 'react-hook-form';
+import { FormField, Modal } from '@tkeel/console-components';
+import { schemas } from '@tkeel/console-utils';
+
+const { TextField } = FormField;
+
+interface FormValues {
+  title: string;
+  admin: {
+    username: string;
+    password?: string;
+    nick_name?: string;
+  };
+  remark?: string;
+}
 
 type Props = {
   isOpen: boolean;
   isConfirmButtonLoading: boolean;
-  formFields: FormFields;
   defaultValues: FormValues;
-  onClose: () => unknown;
-  onConfirm: (formValues: FormValues) => unknown;
+  onClose: () => void;
+  onConfirm: (formValues: FormValues) => void;
 };
 
 export default function ModifyTenantModal({
   isOpen,
   isConfirmButtonLoading,
-  formFields,
   defaultValues,
   onClose,
   onConfirm,
 }: Props) {
+  const {
+    register,
+    formState: { errors },
+    trigger,
+    getValues,
+  } = useForm<FormValues>({
+    defaultValues,
+  });
+
+  const handleConfirm = async () => {
+    const result = await trigger();
+    if (result) {
+      const formValues = getValues();
+      onConfirm(formValues);
+    }
+  };
+
   return (
-    <BaseTenantModal
-      title="编辑"
+    <Modal
+      title="修改租户空间"
       isOpen={isOpen}
       isConfirmButtonLoading={isConfirmButtonLoading}
-      formFields={formFields}
-      defaultValues={defaultValues}
       onClose={onClose}
-      onConfirm={onConfirm}
-    />
+      onConfirm={handleConfirm}
+    >
+      <TextField
+        id="title"
+        label="空间名称"
+        error={errors.title}
+        registerReturn={register('title', schemas.tenantTitle.registerOptions)}
+      />
+      <TextField
+        id="remark"
+        label="备注"
+        error={errors.remark}
+        registerReturn={register('remark')}
+      />
+    </Modal>
   );
 }
