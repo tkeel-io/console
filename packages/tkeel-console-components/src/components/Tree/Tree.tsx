a64d362b50851e92b3fb209d858f66e171dc5421
donniean <acmilank22@gmail.com>
2022-02-20 21:21:14 +0800

    feat: m

--- packages/tkeel-console-components/src/components/Tree/Tree.tsx
+++ packages/tkeel-console-components/src/components/Tree/Tree.tsx
@@ -1,7 +1,30 @@
+import {
+  ChevronDownFilledIcon,
+  ChevronRightFilledIcon,
+} from '@tkeel/console-icons';
+import { merge } from 'lodash';
 import RCTree, { TreeProps } from 'rc-tree';
 
 import 'rc-tree/assets/index.css';
 
+const defaultProps: Partial<TreeProps> = {
+  switcherIcon: (props) => {
+    const { isLeaf, expanded } = props;
+
+    if (isLeaf) {
+      return null;
+    }
+
+    if (expanded) {
+      return <ChevronDownFilledIcon />;
+    }
+
+    return <ChevronRightFilledIcon />;
+  },
+};
+
 export default function Tree(props: Omit<TreeProps, 'prefixCls'>) {
-  return <RCTree prefixCls="rc-tree" {...props} />;
+  const properties = merge({}, defaultProps, props);
+
+  return <RCTree prefixCls="rc-tree" {...properties} />;
 }
