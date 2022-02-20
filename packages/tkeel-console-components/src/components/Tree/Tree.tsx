6fee1d32d48701542bb5af32d46f72973c5e66aa
donniean <acmilank22@gmail.com>
2022-02-21 00:02:41 +0800

    feat: Tree styles (#221)
    * feat: dev PageHeaderToolbar
    
    * feat: m
    
    * feat: dotenv => node-config
    
    * feat: m
    
    * feat: upgrade dependencies
    
    * feat: add platformName to config
    
    * feat: dotenv => node-config
    
    * feat: dotenv => node-config
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add IconButton
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add ActionButtons componnent
    
    * feat: m
    
    * feat: ,
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified eslint rules
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * chore: modified eslint rules
    
    * feat: modified api ip
    
    * feat: add @tkeel/console-plugin-tenant-roles
    
    * feat: sort-package-json
    
    * feat: m
    
    * feat: upgrade dependencies
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add publicPath
    
    * feat:  m
    
    * feat:  m
    
    * feat:  m
    
    * feat:  m
    
    * feat: modify  role
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modify user
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * Update README.md
    
    * Update README.md
    
    * feat:  m
    
    * feat: add Empty
    
    * feat: add Loading
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add docker
    
    * feat: m
    
    * feat: modified scripts
    
    * feat: modified scripts
    
    * feat: readPackages => readPackageInfos
    
    * feat: readPackages => readPackageInfos
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified Modal
    
    * feat: m
    
    * feat:  m
    
    * feat:  m
    
    * feat: m
    
    * feat: add
    
    * feat: m
    
    * Revert "Merge branch 'main' of https://github.com/tkeel-io/console into tkeel-io-main"
    
    This reverts commit 6dafb7a21f49d9a23aac11bf39a1eb9ee644cff5, reversing
    changes made to dbe9774492269f38c9f7f33ce295ddc58ef4bdd7.
    
    * feat: add
    
    * feat:  m
    
    * feat: docker
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: open ESLintPlugin.threads
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add npm scripts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: deleted useless connfig
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat:  modified api (origin)
    
    * feat: m
    
    * feat:  basename => basePath
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: add WebSocket
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: modified webpack config
    
    * feat: upgrade dependencies
    
    * feat: add charts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modifyt enant
    
    * feat: modifyt enant
    
    * feat: m
    
    * feat: delete tenant
    
    * feat: m
    
    * feat: modifyt tenant
    
    * feat: m
    
    * feat: create Tree components
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m

--- packages/tkeel-console-components/src/components/Tree/Tree.tsx
+++ packages/tkeel-console-components/src/components/Tree/Tree.tsx
@@ -1,3 +1,4 @@
+import { Theme, useTheme } from '@chakra-ui/react';
 import {
   ChevronDownFilledIcon,
   ChevronRightFilledIcon,
@@ -5,7 +6,8 @@ import {
 import { merge } from 'lodash';
 import RCTree, { TreeProps } from 'rc-tree';
 
-import 'rc-tree/assets/index.css';
+import { PREFIX_CLS } from './constants';
+import { StyledWrapper } from './styled';
 
 const defaultProps: Partial<TreeProps> = {
   switcherIcon: (props) => {
@@ -16,15 +18,20 @@ const defaultProps: Partial<TreeProps> = {
     }
 
     if (expanded) {
-      return <ChevronDownFilledIcon />;
+      return <ChevronDownFilledIcon size="16px" />;
     }
 
-    return <ChevronRightFilledIcon />;
+    return <ChevronRightFilledIcon size="16px" />;
   },
 };
 
 export default function Tree(props: Omit<TreeProps, 'prefixCls'>) {
   const properties = merge({}, defaultProps, props);
+  const { colors }: Theme = useTheme();
 
-  return <RCTree prefixCls="rc-tree" {...properties} />;
+  return (
+    <StyledWrapper colors={colors}>
+      <RCTree prefixCls={PREFIX_CLS} {...properties} />
+    </StyledWrapper>
+  );
 }
