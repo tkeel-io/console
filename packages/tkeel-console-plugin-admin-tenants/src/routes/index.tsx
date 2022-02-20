81facba9a66e2bebdb74302894f99af8fc59e7dc
donniean <acmilank22@gmail.com>
2022-02-20 15:15:20 +0800

    feat: modifyt tenant

--- packages/tkeel-console-plugin-admin-tenants/src/routes/index.tsx
+++ packages/tkeel-console-plugin-admin-tenants/src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Route, Routes as ReactRouterRoutes } from 'react-router-dom';
 import Index from '../pages/Index';
 import Tenant from '../pages/Tenant';
 
-function Routes() {
+export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<Index />} />
@@ -11,5 +11,3 @@ function Routes() {
     </ReactRouterRoutes>
   );
 }
-
-export default Routes;
