39d61799769af19b30e97146339d34ddeb6ba921
fangshun@ <71168119+fangshun-z@users.noreply.github.com>
2022-02-22 17:55:31 +0800

    merge (#234)
    * feat: create device-template plugin
    
    * feat: add buttons and modals
    
    * feat: modify config
    
    * feat: add Tabs
    
    * feat: modify some components
    
    * feat: add icons
    
    * feat: modify DeviceDetail
    
    * feat: use common components
    
    * feat: modify some style
    
    * feat: use theme color
    
    * feat: use webSocket
    
    * feat: improve DeviceDetail
    
    Co-authored-by: FangShun <davidfang@yunify.com>
    Co-authored-by: donniean <acmilank22@gmail.com>

---
+++ packages/tkeel-console-plugin-tenant-devices/src/pages/DeviceDetail/Components/RawData/index.tsx
@@ -0,0 +1,146 @@
+import { useEffect, useState } from 'react';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Flex,
+  Select,
+  Text,
+} from '@chakra-ui/react';
+import { Editor, SearchInput } from '@tkeel/console-components';
+
+import { RawData } from '@/tkeel-console-plugin-tenant-devices/hooks/queries/useDeviceDetailQuery';
+import useDeviceDetailSocket from '@/tkeel-console-plugin-tenant-devices/hooks/webSockets/useDeviceDetailSocket';
+import { OPTIONS } from '@/tkeel-console-plugin-tenant-devices/pages/DeviceDetail/constants';
+
+type Props = {
+  id: string;
+  initialData?: RawData;
+};
+
+const handleValues = (value: string) => {
+  const str = window.atob(value);
+  return JSON.stringify(str.startsWith('{') ? JSON.parse(str) : str, null, 2);
+};
+
+function Index({ id = '', initialData }: Props) {
+  const initial = {
+    id: '',
+    mark: 'upstream',
+    path: '',
+    ts: 1000,
+    type: '',
+    values: '',
+  } as const;
+  const [rawData, setRawData] = useState<RawData>(initialData || initial);
+  const [keyWord, setKeyWord] = useState('');
+  const result = useDeviceDetailSocket({ id });
+  useEffect(() => {
+    setRawData((preState) => {
+      return { ...preState, ...result.rawData };
+    });
+  }, [result.rawData]);
+  const selectOptions = [
+    {
+      value: 0,
+      label: '文本',
+    },
+    {
+      value: 1,
+      label: '十六进制',
+    },
+  ];
+  const handleSearch = (value: string) => {
+    // eslint-disable-next-line no-console
+    console.log(value, keyWord);
+    setKeyWord(value);
+  };
+
+  const status = OPTIONS[rawData?.mark ?? 'upstream'];
+
+  return (
+    <Box>
+      <Flex align="center" w="100%" h="56px">
+        <Text>原始数据</Text>
+        <Flex flex="1" justifyContent="flex-end">
+          <SearchInput onSearch={handleSearch} placeholder="搜索" />
+        </Flex>
+        <Flex
+          bg="gray.100"
+          borderRadius="70px"
+          pl="10px"
+          minWidth="104px"
+          ml="12px"
+          fontSize="12px"
+          h="32px"
+          fontWeight="600"
+          alignItems="center"
+        >
+          <Select
+            id="json"
+            defaultValue="json"
+            h="32px"
+            fontWeight="600"
+            fontSize="12px"
+            textAlign="center"
+            border="unset"
+            _focus={{ boxShadow: 'none' }}
+          >
+            {selectOptions.map(({ value, label }) => (
+              <option key={value} value={value}>
+                {label}
+              </option>
+            ))}
+          </Select>
+        </Flex>
+      </Flex>
+      <Accordion allowMultiple p="12px 12px" bg="gray.50">
+        <AccordionItem
+          borderWidth="1px"
+          borderColor="gray.200"
+          borderRadius="4px"
+          bg="white"
+          mb="12px"
+          p="10px 12px 10px 20px"
+        >
+          <AccordionButton _focus={{ boxShadow: 'none' }} p="unset">
+            <Flex flex="1" fontSize="12px" alignItems="center">
+              <Box
+                bg={status.bg}
+                color={status.color}
+                w="42px"
+                h="24px"
+                textAlign="center"
+                borderRadius="2px"
+                fontWeight="600"
+                lineHeight="2"
+              >
+                {status.desc}
+              </Box>
+              <Text fontWeight="700" m="0 38px 0 22px">
+                {rawData.path}
+              </Text>
+              <Text color="grayAlternatives.300">2022-11-02 12:32:12</Text>
+            </Flex>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>
+            <Editor
+              theme="light"
+              value={handleValues(rawData?.values || '')}
+              language="json"
+              readOnly
+              width="100%"
+              height="144px"
+            />
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
+  );
+}
+
+export default Index;
