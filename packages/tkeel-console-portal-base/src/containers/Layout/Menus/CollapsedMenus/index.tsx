40e8690d9351616d593c50be62136b6c1145be0b
donniean <acmilank22@gmail.com>
2022-02-22 11:48:35 +0800

    feat: m

--- packages/tkeel-console-portal-base/src/containers/Layout/Menus/CollapsedMenus/index.tsx
+++ packages/tkeel-console-portal-base/src/containers/Layout/Menus/CollapsedMenus/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Center, Flex, Image } from '@chakra-ui/react';
 
-import Logo from '@/tkeel-console-portal-base/assets/images/logo.svg';
+import Logo from '@/tkeel-console-portal-base/assets/images/logo-tkeel.svg';
 import useMenusQuery from '@/tkeel-console-portal-base/hooks/queries/useMenusQuery';
 
 // import MenuItem from './MenuItem';
