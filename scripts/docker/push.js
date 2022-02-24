2d96e7c27bb94ef77dd851ec3c803427f7aa0b8a
donniean <acmilank22@gmail.com>
2022-02-24 15:50:47 +0800

    feat: add scripts (#259)
    * feat: m
    
    * feat: m
    
    * feat: modify user
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * Update README.md
    
    * Update README.md
    
    * feat:  m
    
    * feat: add Empty
    
    * feat: add Loading
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add docker
    
    * feat: m
    
    * feat: modified scripts
    
    * feat: modified scripts
    
    * feat: readPackages => readPackageInfos
    
    * feat: readPackages => readPackageInfos
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified Modal
    
    * feat: m
    
    * feat:  m
    
    * feat:  m
    
    * feat: m
    
    * feat: add
    
    * feat: m
    
    * Revert "Merge branch 'main' of https://github.com/tkeel-io/console into tkeel-io-main"
    
    This reverts commit 6dafb7a21f49d9a23aac11bf39a1eb9ee644cff5, reversing
    changes made to dbe9774492269f38c9f7f33ce295ddc58ef4bdd7.
    
    * feat: add
    
    * feat:  m
    
    * feat: docker
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: open ESLintPlugin.threads
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add npm scripts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: deleted useless connfig
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat:  modified api (origin)
    
    * feat: m
    
    * feat:  basename => basePath
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: add WebSocket
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: modified webpack config
    
    * feat: upgrade dependencies
    
    * feat: add charts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modifyt enant
    
    * feat: modifyt enant
    
    * feat: m
    
    * feat: delete tenant
    
    * feat: m
    
    * feat: modifyt tenant
    
    * feat: m
    
    * feat: create Tree components
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * Merge branch 'main' of https://github.com/donniean/console
    
    # 请输入一个提交信息以解释此合并的必要性，尤其是将一个更新后的上游分支
    # 合并到主题分支。
    #
    # 以 '#' 开始的行将被忽略，而空的提交说明将终止提交。
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: move TreeDemo
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat:  m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: upgrade versions
    
    * feat: m
    
    * feat:  m

--- scripts/docker/push.js
+++ scripts/docker/push.js
@@ -1,6 +1,7 @@
 const shell = require('shelljs');
 
 const prompt = require('./prompt');
+const setChartVersions = require('./set-chart-versions');
 
 (async () => {
   const infos = await prompt();
@@ -8,4 +9,5 @@ const prompt = require('./prompt');
     const command = `docker image push ${name}`;
     shell.exec(command);
   });
+  setChartVersions(infos);
 })();
---
+++ scripts/docker/set-chart-versions.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const fs = require('fs-extra');
+const yaml = require('js-yaml');
+const _ = require('lodash');
+
+const paths = require('../utils/paths');
+
+const chartsDirectoryPath = paths.resolveRoot('charts');
+const files = [
+  { path: 'Chart.yaml', valuePath: 'version' },
+  { path: 'values.yaml', valuePath: 'image.tag' },
+];
+
+function getDirectoryNameByDockerRepository(repository = '') {
+  return repository.replace(/^tkeelio\//, '');
+}
+
+module.exports = async function setChartVersions(imageInfos) {
+  imageInfos.forEach(({ Repository, Tag }) => {
+    const directoryName = getDirectoryNameByDockerRepository(Repository);
+    const directoryPath = path.resolve(chartsDirectoryPath, directoryName);
+    const isExists = fs.existsSync(directoryPath);
+
+    if (isExists) {
+      files.forEach((file) => {
+        const filePath = path.resolve(directoryPath, file.path);
+        const yamlContent = fs.readFileSync(filePath);
+        const json = yaml.load(yamlContent);
+        let obj = null;
+
+        if (file.path === 'Chart.yaml') {
+          obj = {
+            version: Tag,
+          };
+        } else if (file.path === 'values.yaml') {
+          obj = {
+            image: {
+              tag: Tag,
+            },
+          };
+        }
+
+        const newJson = _.merge({}, json, obj);
+        const newYamlContent = yaml.dump(newJson);
+        fs.writeFileSync(filePath, newYamlContent);
+      });
+    }
+  });
+};
