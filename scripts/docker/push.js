2b4dd8015870ab5bf3c5b809698a4077a12da384
donniean <acmilank22@gmail.com>
2022-02-24 15:47:33 +0800

    feat:  m

--- scripts/docker/push.js
+++ scripts/docker/push.js
@@ -1,14 +1,13 @@
-// const shell = require('shelljs');
+const shell = require('shelljs');
 
 const prompt = require('./prompt');
 const setChartVersions = require('./set-chart-versions');
 
 (async () => {
   const infos = await prompt();
-  setChartVersions(infos);
-  /* infos.forEach(({ name }) => {
+  infos.forEach(({ name }) => {
     const command = `docker image push ${name}`;
-
     shell.exec(command);
-  }); */
+  });
+  setChartVersions(infos);
 })();
