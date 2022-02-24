05b62c2ef7d559fa376453b5e4e107c2906edf8c
zhanghan <751818302zh@gmail.com>
2022-02-24 17:39:10 +0800

    feat: change config

--- packages/tkeel-console-plugin-tenant-data-subscription/config/default.js
+++ packages/tkeel-console-plugin-tenant-data-subscription/config/default.js
@@ -1,3 +1,5 @@
+const { tkeel } = require('../../../config/default');
+
 module.exports = {
   publicPath: '/static/console-plugin-tenant-data-subscription/',
   basePath: '/tenant-data-subscription',
@@ -13,8 +15,23 @@ module.exports = {
   plugin: {
     identify: {
       plugin_id: 'console-plugin-tenant-data-subscription',
-      entries: [],
-      dependence: [],
+      entries: [
+        {
+          id: 'tenant-data-use',
+          name: '数据使用',
+          icon: 'PuzzleTwoToneIcon',
+          children: [
+            {
+              id: 'console-plugin-tenant-data-subscription',
+              name: '数据订阅',
+              path: '/tenant-data-subscription',
+              entry: '/static/console-plugin-tenant-data-subscription/',
+              portal: 1,
+            },
+          ],
+        },
+      ],
+      dependence: [{ id: 'core-broker', version: tkeel.version }],
     },
   },
 };
