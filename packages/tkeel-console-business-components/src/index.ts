e85dcb76c183521bd78f867d7826d82fc5be1c43
donniean <acmilank22@gmail.com>
2022-03-01 20:12:51 +0800

    feat: m

--- packages/tkeel-console-business-components/src/index.ts
+++ packages/tkeel-console-business-components/src/index.ts
@@ -3,13 +3,6 @@ export { default as DeveloperInfo } from './components/DeveloperInfo';
 export { default as NotFound } from './components/NotFound';
 export { default as PluginCard } from './components/PluginCard';
 export { default as PluginNum } from './components/PluginNum';
-export {
-  default as PluginProvider,
-  useGlobalPluginProps,
-} from './components/PluginProvider';
-export {
-  GlobalPortalProps,
-  default as PortalProvider,
-  useGlobalPortalProps,
-} from './components/PortalProvider';
+export * from './components/PluginProvider';
+export * from './components/PortalProvider';
 export { default as SetPasswordModal } from './components/SetPasswordModal';
