4811200a61eafc95edc2d42c0a8420a0943e6485
donniean <acmilank22@gmail.com>
2022-03-01 18:17:19 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-tenants/src/hooks/useQueries/index.ts
+++ packages/tkeel-console-plugin-admin-tenants/src/hooks/useQueries/index.ts
@@ -1,18 +1,13 @@
 import { merge } from 'lodash';
 
-import { useGlobalPluginProps } from '@tkeel/console-business-components';
 import {
   UseCustomQueryOptions,
-  useNoAuthRedirectPath,
+  usePluginHandleNoAuth,
   useQueries as useCustomQueries,
 } from '@tkeel/console-hooks';
-import { createHandleNoAuth } from '@tkeel/console-utils';
 
 export default function useQueries(optionsList: UseCustomQueryOptions[]) {
-  const { portalName, navigate } = useGlobalPluginProps();
-  const basePath = process.env.BASE_PATH;
-  const redirectPath = useNoAuthRedirectPath({ portalName, basePath });
-  const handleNoAuth = createHandleNoAuth({ navigate, redirectPath });
+  const handleNoAuth = usePluginHandleNoAuth();
   const optsList = optionsList.map((option) => {
     return merge({}, { extras: { handleNoAuth } }, option);
   });
--- packages/tkeel-console-plugin-admin-tenants/src/hooks/useQuery/index.ts
+++ packages/tkeel-console-plugin-admin-tenants/src/hooks/useQuery/index.ts
@@ -1,22 +1,17 @@
 import { merge } from 'lodash';
 
-import { useGlobalPluginProps } from '@tkeel/console-business-components';
 import {
   UseCustomQueryOptions,
-  useNoAuthRedirectPath,
+  usePluginHandleNoAuth,
   useQuery as useCustomQuery,
 } from '@tkeel/console-hooks';
-import { createHandleNoAuth } from '@tkeel/console-utils';
 
 export default function useQuery<
   TApiData,
   TRequestParams = undefined,
   TRequestData = undefined
 >(options: UseCustomQueryOptions<TApiData, TRequestParams, TRequestData>) {
-  const { portalName, navigate } = useGlobalPluginProps();
-  const { basePath } = GLOBAL_PLUGIN_CONFIG;
-  const redirectPath = useNoAuthRedirectPath({ portalName, basePath });
-  const handleNoAuth = createHandleNoAuth({ navigate, redirectPath });
+  const handleNoAuth = usePluginHandleNoAuth();
   const opts = merge({}, { extras: { handleNoAuth } }, options);
 
   return useCustomQuery<TApiData, TRequestParams, TRequestData>(opts);
