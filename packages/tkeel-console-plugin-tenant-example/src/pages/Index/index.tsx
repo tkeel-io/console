31483425df61d418fbbe4b4e0a9d8cac778f1913
donniean <acmilank22@gmail.com>
2022-03-01 19:39:24 +0800

    feat: m

--- packages/tkeel-console-plugin-tenant-example/src/pages/Index/index.tsx
+++ packages/tkeel-console-plugin-tenant-example/src/pages/Index/index.tsx
@@ -1,12 +1,9 @@
 import { Box, Heading } from '@chakra-ui/react';
 
-import TreeSelectExample from './components/TreeSelectExample';
-
 export default function Index() {
   return (
-    <Box bg="white" w="100%" h="100%">
+    <Box>
       <Heading>Example</Heading>
-      <TreeSelectExample />
     </Box>
   );
 }
