01f7f74ca8f462c676e1e5dca83c3d144f0ede64
donniean <acmilank22@gmail.com>
2022-03-01 20:03:39 +0800

    feat:  m

--- packages/tkeel-console-business-components/src/components/PluginProvider/useGlobalPluginProps.ts
+++ packages/tkeel-console-business-components/src/components/PluginProvider/useGlobalPluginProps.ts
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 
 import Context from './context';
 
-export default function useGlobalProps() {
+export default function useGlobalPluginProps() {
   return useContext(Context);
 }
