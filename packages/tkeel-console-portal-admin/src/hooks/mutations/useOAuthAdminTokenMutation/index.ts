d3d6b4e6d9cee727fee646dcf79756be7b2d8bef
donniean <acmilank22@gmail.com>
2022-03-01 13:58:20 +0800

    feat: split portals (#299)
    * feat: m
    
    * feat: open ESLintPlugin.threads
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add npm scripts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: deleted useless connfig
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat:  modified api (origin)
    
    * feat: m
    
    * feat:  basename => basePath
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: add WebSocket
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: modified webpack config
    
    * feat: upgrade dependencies
    
    * feat: add charts
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modifyt enant
    
    * feat: modifyt enant
    
    * feat: m
    
    * feat: delete tenant
    
    * feat: m
    
    * feat: modifyt tenant
    
    * feat: m
    
    * feat: create Tree components
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * Merge branch 'main' of https://github.com/donniean/console
    
    # 请输入一个提交信息以解释此合并的必要性，尤其是将一个更新后的上游分支
    # 合并到主题分支。
    #
    # 以 '#' 开始的行将被忽略，而空的提交说明将终止提交。
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: move TreeDemo
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat:  m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: upgrade versions
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: add loading
    
    * feat: add Skeleton on load plugin
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: upgrade dependencies
    
    * feat: upgrade dependencies
    
    * style: modified eslint-plugin-simple-import-sort rules
    
    * style: modified eslint-plugin-simple-import-sort rules
    
    * feat: upgrade dependencies
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: split portals

---
+++ packages/tkeel-console-portal-admin/src/hooks/mutations/useOAuthAdminTokenMutation/index.ts
@@ -0,0 +1,27 @@
+import useMutation from '@/tkeel-console-portal-admin/hooks/useMutation';
+
+interface RequestParams {
+  password?: string;
+}
+
+export interface ApiData {
+  '@type': string;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+  token_type: string;
+}
+
+const url = '/rudder/v1/oauth2/admin';
+const method = 'GET';
+
+export default function useOAuthAdminTokenMutation() {
+  return useMutation<ApiData, RequestParams>({
+    url,
+    method,
+    extras: {
+      isWithToken: false,
+      handleNoAuth: false,
+    },
+  });
+}
--- packages/tkeel-console-portal-base/src/contexts/GlobalPropsContext/types.ts
+++ packages/tkeel-console-portal-base/src/contexts/GlobalPropsContext/types.ts
@@ -1,7 +1,5 @@
-import { PlatformNames } from '@tkeel/console-constants';
 import { ThemeNames } from '@tkeel/console-themes';
 
 export interface GlobalProps {
-  platformName: PlatformNames;
   themeName: ThemeNames;
 }
