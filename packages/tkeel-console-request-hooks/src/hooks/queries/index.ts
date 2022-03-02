f237b387a91ce98df22e94118307b34ba8d94118
donniean <acmilank22@gmail.com>
2022-03-03 02:17:06 +0800

    feat: merge useRolesQuery

--- packages/tkeel-console-request-hooks/src/hooks/queries/index.ts
+++ packages/tkeel-console-request-hooks/src/hooks/queries/index.ts
@@ -1,4 +1,6 @@
 export { default as usePortalAdminConfigQuery } from './usePortalAdminConfigQuery';
 export { default as usePortalTenantConfigQuery } from './usePortalTenantConfigQuery';
+export { default as useRolesQuery } from './useRolesQuery';
+export * from './useRolesQuery';
 export { default as useUsersQuery } from './useUsersQuery';
 export * from './useUsersQuery';
