f067e810a7aede28ff7e6a85fcdc8e0ee8902c0b
fangshun@ <71168119+fangshun-z@users.noreply.github.com>
2022-03-02 12:03:21 +0800

    merge (#308)
    * feat: create device-template plugin
    
    * feat: add buttons and modals
    
    * feat: modify config
    
    * feat: add Tabs
    
    * feat: modify some components
    
    * feat: add icons
    
    * feat: modify DeviceDetail
    
    * feat: use common components
    
    * feat: modify some style
    
    * feat: use theme color
    
    * feat: use webSocket
    
    * feat: improve DeviceDetail
    
    * feat: modify RawData
    
    * feat: modify useCancelSubscribeMutation
    
    * feat: modify UnsubscribeButton
    
    * feat: modify RawData
    
    * feat: modify some bugs
    
    * feat: modify DeviceBasicInfoCard
    
    * feat: modify timestamp
    
    * feat: modidy RawData
    
    * feat: add buttons
    
    * feat: add AddSubscribeModal
    
    Co-authored-by: FangShun <davidfang@yunify.com>
    Co-authored-by: donniean <acmilank22@gmail.com>

---
+++ packages/tkeel-console-plugin-tenant-devices/src/components/AddSubscribeButton/index.tsx
@@ -0,0 +1,54 @@
+import { useDisclosure } from '@chakra-ui/react';
+
+import { MoreActionButton, toast } from '@tkeel/console-components';
+import { AddFilledIcon } from '@tkeel/console-icons';
+
+import AddSubscribeModal, {
+  FormValues,
+} from '@/tkeel-console-plugin-tenant-devices/components/AddSubscribeModal';
+import useSubscribeByDeviceMutation from '@/tkeel-console-plugin-tenant-devices/hooks/mutations/useSubscribeByDeviceMutation';
+
+type Props = {
+  id: string;
+  refetch?: () => void;
+};
+
+function AddSubscribeButton({ id, refetch }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { mutate, isLoading } = useSubscribeByDeviceMutation({
+    deviceId: id,
+    onSuccess() {
+      toast({ status: 'success', title: '添加成功' });
+      onClose();
+      if (refetch) {
+        refetch();
+      }
+    },
+  });
+
+  const handleConfirm = (formValues: FormValues) => {
+    mutate({
+      data: formValues,
+    });
+  };
+
+  return (
+    <>
+      <MoreActionButton
+        onClick={onOpen}
+        icon={<AddFilledIcon size="12px" />}
+        title="添加订阅"
+      />
+      {isOpen && (
+        <AddSubscribeModal
+          isConfirmButtonLoading={isLoading}
+          isOpen={isOpen}
+          onClose={onClose}
+          onConfirm={handleConfirm}
+        />
+      )}
+    </>
+  );
+}
+
+export default AddSubscribeButton;
