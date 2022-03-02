779c263fae219738dbb76e9f6543798a57482291
jackli <jackli@yunify.com>
2022-03-02 23:41:43 +0800

    feat: data-query searchResult request data

--- packages/tkeel-console-plugin-tenant-data-query/src/pages/SearchResult/index.tsx
+++ packages/tkeel-console-plugin-tenant-data-query/src/pages/SearchResult/index.tsx
@@ -1,11 +1,106 @@
 import { Flex, Grid, Text } from '@chakra-ui/react';
+// import { Base64 } from 'js-base64';
+import { useSearchParams } from 'react-router-dom';
 
 import { PageHeaderToolbar } from '@tkeel/console-components';
 
 import DeviceInfoCard from '@/tkeel-console-plugin-tenant-data-query/components/DeviceInfoCard';
+import useDeviceListQuery from '@/tkeel-console-plugin-tenant-data-query/hooks/queries/useDeviceListQuery';
 import SearchDeviceInput from '@/tkeel-console-plugin-tenant-data-query/pages/Index/components/SearchDeviceInput';
+import {
+  DEVICE_GROUP_ID,
+  DEVICE_TEMPLATES_ID,
+} from '@/tkeel-console-plugin-tenant-data-query/pages/Index/constants';
+import { RequestDataCondition } from '@/tkeel-console-plugin-tenant-data-query/types/request-data';
 
 export default function SearchResult() {
+  const [searchParams] = useSearchParams();
+
+  const baseRequestData = {
+    query: '',
+    page_num: 1,
+    page_size: 1000,
+    order_by: 'name',
+    is_descending: false,
+  };
+  const statusQueryField = 'connectInfo._online';
+  const deviceGroupIdQueryField = 'sysField._spacePath';
+  const templateIdQueryField = 'basicInfo.templateId';
+
+  const conditions: RequestDataCondition[] = [
+    {
+      field: 'type',
+      operator: '$eq',
+      value: 'device',
+    },
+  ];
+
+  const groupId = searchParams.get('group-id');
+  if (groupId) {
+    conditions.push({
+      field: deviceGroupIdQueryField,
+      operator: '$wildcard',
+      value: groupId,
+    });
+  }
+
+  const templateId = searchParams.get('template-id');
+  if (templateId) {
+    conditions.push({
+      field: templateIdQueryField,
+      operator: '$wildcard',
+      value: templateId,
+    });
+  }
+
+  const status = searchParams.get('status') || '';
+  if (['online', 'offline'].includes(status)) {
+    conditions.push({
+      field: statusQueryField,
+      operator: '$eq',
+      value: status === 'online',
+    });
+  }
+
+  const keywords = searchParams.get('keywords') || '';
+
+  const { deviceList, isLoading } = useDeviceListQuery({
+    requestData: {
+      ...baseRequestData,
+      query: keywords,
+      condition: conditions,
+    },
+  });
+
+  // eslint-disable-next-line no-console
+  console.log('SearchResult ~ isLoading', isLoading);
+  const defaultFilterConditions = [];
+
+  const groupName = decodeURIComponent(searchParams.get('group-name') || '');
+  const templateName = decodeURIComponent(
+    searchParams.get('template-name') || ''
+  );
+  if (groupName) {
+    defaultFilterConditions.push({
+      id: DEVICE_GROUP_ID,
+      label: '设备分组',
+      value: groupName,
+    });
+  } else if (templateName) {
+    defaultFilterConditions.push({
+      id: DEVICE_TEMPLATES_ID,
+      label: '设备模板',
+      value: templateName,
+    });
+  }
+  if (keywords) {
+    defaultFilterConditions.push({
+      id: 'keywords',
+      label: '关键字',
+      value: keywords,
+    });
+  }
+
   return (
     <Flex height="100%" flexDirection="column">
       <Flex justifyContent="flex-start" alignItems="center">
@@ -14,7 +109,11 @@ export default function SearchResult() {
           hasIcon
           styles={{ wrapper: { width: 'auto' } }}
         />
-        <SearchDeviceInput style={{ marginLeft: '19%' }} type="searchResult" />
+        <SearchDeviceInput
+          defaultFilterConditions={defaultFilterConditions}
+          style={{ marginLeft: '19%' }}
+          type="searchResult"
+        />
       </Flex>
       <Flex marginTop="16px" fontSize="12px" color="gray.800" lineHeight="24px">
         共
@@ -31,12 +130,9 @@ export default function SearchResult() {
         gap="8px"
         overflowY="auto"
       >
-        {Array.from({ length: 20 })
-          .fill('')
-          .map((_, i) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <DeviceInfoCard key={i} />
-          ))}
+        {deviceList.map((device) => (
+          <DeviceInfoCard key={device.id} device={device} />
+        ))}
       </Grid>
     </Flex>
   );
