949521a321228a290c96eae57ec17a9f158d4b7d
fangshun-z <3181076477@qq.com>
2022-03-03 10:14:16 +0800

    feat: delete some components

--- packages/tkeel-console-plugin-tenant-device-templates/src/hooks/mutations/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as useCreateTemplateMutation } from './useCreateTemplateMutation';
-export { default as useDeleteTemplateMutation } from './useDeleteTemplateMutation';
-export { default as useSetTemplatePermissionsMutation } from './useSetTemplatePermissionsMutation';
