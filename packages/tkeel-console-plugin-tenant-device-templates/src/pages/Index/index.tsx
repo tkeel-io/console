441e10364d61671fa5eceba152f69512819841a7
zhanghan <51943297+ZhangHOOO@users.noreply.github.com>
2022-03-10 18:45:35 +0800

    feat：add model (#375)
    * feat: change template
    
    * feat: merge main
    
    * feat: add  template
    
    * feat: add model
    
    * feat: fixed reflect

--- packages/tkeel-console-plugin-tenant-device-templates/src/pages/Index/index.tsx
+++ packages/tkeel-console-plugin-tenant-device-templates/src/pages/Index/index.tsx
@@ -1,12 +1,13 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { useState } from 'react';
 
+import { TemplateCard } from '@tkeel/console-business-components';
 import { PageHeaderToolbar } from '@tkeel/console-components';
-// import useTemplatesQuery from '@/tkeel-console-plugin-tenant-device-templates/hooks/queries/useTemplatesQuery';
+import { BoxTwoToneIcon } from '@tkeel/console-icons';
 import { useTemplateQuery } from '@tkeel/console-request-hooks';
 import { plugin } from '@tkeel/console-utils';
 
-import { CreateTemplateButton } from './components/buttons';
+import CreateTemplateButton from '@/tkeel-console-plugin-tenant-device-templates/pages/Index/components/CreateTemplateButton';
 
 function Index() {
   const toast = plugin.getPortalToast();
@@ -25,9 +26,9 @@ function Index() {
     ],
   };
 
-  const { items } = useTemplateQuery(defaultParams);
+  const { keyData } = useTemplateQuery(defaultParams);
   // eslint-disable-next-line no-console
-  console.log('Index ~ items', items);
+  console.log('Index ~ keyData', keyData);
 
   const [keyWord, setKeyWord] = useState('');
   // eslint-disable-next-line no-console
@@ -54,6 +55,51 @@ function Index() {
           <CreateTemplateButton key="create" onSuccess={handleCreateSuccess} />,
         ]}
       />
+
+      <Box
+        bg="gray.50"
+        boxShadow="0px 8px 8px rgba(152, 163, 180, 0.1)"
+        borderRadius="4px"
+        mt="20px"
+        padding="20px 0"
+      >
+        <Flex flexWrap="wrap" paddingLeft="20px">
+          {keyData.map((item) => {
+            return (
+              <TemplateCard
+                key={item.id}
+                icon={
+                  <BoxTwoToneIcon style={{ width: '24px', height: '22px' }} />
+                }
+                title={item.title}
+                description={item.description}
+                navigateUrl={`/detail/${item.id}`}
+                // buttons={[
+                //   <ModifySubscriptionButton
+                //     data={item}
+                //     key="modify"
+                //     onSuccess={() => {
+                //       // refetch();
+                //     }}
+                //   />,
+                //   <DeleteSubscriptionButton
+                //     key="delete"
+                //     id={item.id}
+                //     name={item.title}
+                //     refetchData={() => {
+                //       // refetch();
+                //     }}
+                //   />,
+                // ]}
+                footer={[
+                  // { name: '使用设备', value: item.id },
+                  { name: '最新时间', value: item.updatedAt as string },
+                ]}
+              />
+            );
+          })}
+        </Flex>
+      </Box>
     </Flex>
   );
 }
