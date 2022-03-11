1320a28c6c3eae8e71cfc754a3b60e3b1ffd81a3
donniean <acmilank22@gmail.com>
2022-03-11 12:58:51 +0800

    feat: add Redirect

---
+++ packages/tkeel-console-portal-tenant/src/pages/Redirect/index.tsx
@@ -0,0 +1,9 @@
+// import { useSearchParams } from 'react-router-dom';
+
+export default function Redirect() {
+  // const [searchParams] = useSearchParams();
+
+  // console.log(searchParams, searchParams.get('a'));
+
+  return null;
+}
--- packages/tkeel-console-portal-tenant/src/routes/notRequireAuthRoutes/index.tsx
+++ packages/tkeel-console-portal-tenant/src/routes/notRequireAuthRoutes/index.tsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { NotFound } from '@tkeel/console-business-components';
 
 import Login from '@/tkeel-console-portal-tenant/pages/Login';
+import Redirect from '@/tkeel-console-portal-tenant/pages/Redirect';
 import SetPassword from '@/tkeel-console-portal-tenant/pages/SetPassword';
 import Tenant from '@/tkeel-console-portal-tenant/pages/Tenant';
 
@@ -12,6 +13,7 @@ export default (
     <Route path="login" element={<Login />}>
       <Route path=":tenantId" />
     </Route>
+    <Route path="redirect" element={<Redirect />} />
     <Route path="set-password" element={<SetPassword />} />
     <Route path="*" element={<NotFound />} />
   </>
