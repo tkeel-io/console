5c75cb7cedbc99adee1dade498da697699c02c5e
jackzerone <74034572+jackzerone@users.noreply.github.com>
2022-03-11 14:15:59 +0800

    chore: modify plugin detail (#384)
    * chore: replace tKeel with primary
    
    * feat: menu add MenuTheme & button add solid-no-shadow variant
    
    * chore: modify SearchInput
    
    * chore: modify install button style
    
    * feat: add FormField
    
    * feat: replace Login FormControl
    
    * chore: modify TextField
    
    * feat: selectField add onChange
    
    * chore: modify SelectField default placeholder
    
    * chore: modify input placeholder color
    
    * chore: modify EnablePluginList
    
    * feat: table add scroll & selection & sort
    
    * chore: modify Tabel props
    
    * chore: add theme color
    
    * chore: modify table Head props
    
    * feat: add queries & mutations
    
    * chore: modify useQuery & useMutation
    
    * feat: add useRepoInstallersQueries
    
    * chore: modify colors
    
    * feat: modal add modalBodyStyle
    
    * chore: modify Modal style
    
    * feat: add tenant-plugins
    
    * feat: init tenant-plugins index page
    
    * feat: add usePluginsTenantsQuery
    
    * chore: merge tkeel-io:main
    
    * feat: plugin content setTotalSize
    
    * feat: add plugins refetch
    
    * feat: add MoreAction & MoreActionButton
    
    * feat: add RectangleButton & Drawer
    
    * feat: init DisableButton
    
    * feat: modify EnableButton & DisableButton
    
    * feat: modify Menus
    
    * feat: plugin detail add DisableButton
    
    * feat: add BackButton
    
    * chore: modify admin-plugins default.js
    
    * feat: add InfoCard
    
    * feat: add CustomTab
    
    * feat: add CustomTabList
    
    * feat: table add isShowStripe props
    
    * chore: modify Menus
    
    * feat: modify Menus
    
    * feat: add MenuIcon
    
    * feat: modify Table types
    
    * feat: modify Menus & add useColor
    
    * feat: modify MoreAction props
    
    * chore: add console-plugin-tenant-data-query
    
    * feat: add BasicInfo component
    
    * feat: modify usePluginDetailQuery
    
    * feat: add DeviceInfoCard
    
    * feat: add qingcloud logo
    
    * feat: add DeviceGroup
    
    * feat: modify InfoCard
    
    * feat: add FilterCondition
    
    * chore: modify MoreAction
    
    * chore: modify MoreAction
    
    * chore: modify SearchDeviceInput
    
    * feat: add DeviceList
    
    * feat: add SearchResult
    
    * chore: hidden toggle menu theme icon
    
    * chore: modify ExpandMenus
    
    * chore: modify usePagination
    
    * chore: modify PluginCard
    
    * chore: modify CreateButton style
    
    * chore: modify SubMenuLink style
    
    * chore: install plugin add loading
    
    * feat: globalProps add refetchMenus
    
    * chore: replace qingcloud-logo
    
    * chore: modify admin-plugin Content
    
    * chore: modify Breadcrumb
    
    * feat: portal-base TextField add FormControlStyle
    
    * chore: modify StatusSelect
    
    * chore: merge tkeel-io:main
    
    * chore: merge tkeel-io:main
    
    * feat: add useDeviceListQuery
    
    * chore: modify menu style
    
    * feat: add NoData
    
    * feat: searchInput add onChange
    
    * feat: replace plugin installed with state
    
    * feat: modify pluginNumData
    
    * chore: get logo from config
    
    * feat: data-query searchResult request data
    
    * chore: modify admin-plugin style
    
    * chore: merge tkeel-io:main
    
    * feat: add setLocalMenuCollapsed
    
    * chore: modify button style
    
    * fix: table selection
    
    * chore: modify table style
    
    * chore: remove test code
    
    * chore: modify MoreAction
    
    * fix: pagination pageSizeArr
    
    * chore: modify CustomTab
    
    * chore: modify tab style
    
    * chore: modify menu style
    
    * chore: add no-plugins
    
    * chore: modify layout
    
    * chore: modify data-query detail
    
    * feat: add DateRangeIndicator
    
    * chore: modify DateRangeIndicator
    
    * chore: modify detail
    
    * feat: add DateRangePicker
    
    * feat: modify Detail
    
    * chore: modify EnablePluginList
    
    * feat: modify admin-plugin Detail

--- packages/tkeel-console-plugin-admin-plugins/src/pages/Detail/index.tsx
+++ packages/tkeel-console-plugin-admin-plugins/src/pages/Detail/index.tsx
@@ -21,7 +21,7 @@ function Detail() {
   const [searchParams] = useSearchParams();
   const name = searchParams.get('name') || '';
 
-  const { pluginDetail, refetch } = usePluginDetailQuery({
+  const { pluginDetail, isSuccess, refetch } = usePluginDetailQuery({
     repoName: searchParams.get('repo') || '',
     installerName: name,
     installerVersion: searchParams.get('version') || '',
@@ -29,7 +29,11 @@ function Detail() {
 
   const readme = pluginDetail?.metadata?.readme ?? '';
   const maintainers = pluginDetail?.maintainers ?? [];
-  const uninstall = pluginDetail?.state === 'UNINSTALL';
+  let uninstall = true;
+  if (isSuccess) {
+    uninstall = pluginDetail?.state === 'UNINSTALL';
+  }
+
   return (
     <Flex height="100%" paddingBottom="20px" justifyContent="space-between">
       <Box width="360px" flexShrink="0">
