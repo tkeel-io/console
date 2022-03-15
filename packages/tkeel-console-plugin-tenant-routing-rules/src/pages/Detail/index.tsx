372fbf2dffc7fa5712bfb88a6a83a5c9b506cf8f
jackli <jackli@yunify.com>
2022-03-15 22:40:06 +0800

    chore: add detail title

--- packages/tkeel-console-plugin-tenant-routing-rules/src/pages/Detail/index.tsx
+++ packages/tkeel-console-plugin-tenant-routing-rules/src/pages/Detail/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Square, Text } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
 import { BackButton } from '@tkeel/console-components';
+
+import TextWrapper from './TextWrapper';
 
 export default function Detail() {
   const navigate = useNavigate();
@@ -16,7 +18,7 @@ export default function Detail() {
         position="absolute"
         left="0"
         top="0"
-        width="100%"
+        width="calc(100% + 20px)"
         height="200px"
         backgroundColor="grayAlternatives.50"
       />
@@ -28,7 +30,35 @@ export default function Detail() {
           navigate('/');
         }}
       />
-      <Flex width="82%">ss</Flex>
+      <Flex position="relative" flexDirection="column" width="82%">
+        <Flex justifyContent="space-between">
+          <Flex alignItems="center">
+            <Square size="40px" backgroundColor="gray.50" />
+            <Text
+              marginLeft="8px"
+              color="gray.700"
+              fontSize="18px"
+              fontWeight="600"
+              lineHeight="24px"
+            >
+              数据存储方案B
+            </Text>
+          </Flex>
+        </Flex>
+        <TextWrapper
+          label="描述"
+          value="模型说明一句话描述模型说明一句话描述模型说明一句话描述模型说明一句话模型描述明"
+          styles={{ wrapper: { marginTop: '8px' } }}
+        />
+        <Flex marginTop="8px">
+          <TextWrapper label="创建时间" value="2021-12-01 13:41" />
+          <TextWrapper
+            label="修改时间"
+            value="2022-03-01 15:15"
+            styles={{ wrapper: { marginLeft: '20px' } }}
+          />
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
