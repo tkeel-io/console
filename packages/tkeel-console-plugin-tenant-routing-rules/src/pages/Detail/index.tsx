1d7db745fae907108f8677e7eb802e37f281db16
jackli <jackli@yunify.com>
2022-03-15 15:03:56 +0800

    feat: detail add BackButton

--- packages/tkeel-console-plugin-tenant-routing-rules/src/pages/Detail/index.tsx
+++ packages/tkeel-console-plugin-tenant-routing-rules/src/pages/Detail/index.tsx
@@ -1,5 +1,34 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+import { BackButton } from '@tkeel/console-components';
 
 export default function Detail() {
-  return <Box>Detail</Box>;
+  const navigate = useNavigate();
+  return (
+    <Flex
+      paddingTop="20px"
+      height="100%"
+      justifyContent="center"
+      position="relative"
+    >
+      <Box
+        position="absolute"
+        left="0"
+        top="0"
+        width="100%"
+        height="200px"
+        backgroundColor="grayAlternatives.50"
+      />
+      <BackButton
+        position="absolute"
+        left="37px"
+        top="20px"
+        onClick={() => {
+          navigate('/');
+        }}
+      />
+      <Flex width="82%">ss</Flex>
+    </Flex>
+  );
 }
