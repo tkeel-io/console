2cf6609ca7bf72bc63ab95be789d5997449be95e
lunz1207 <lunz1207@gmail.com>
2022-03-18 15:33:24 +0800

    chore: cspell

--- cspell.config.js
+++ cspell.config.js
@@ -55,6 +55,12 @@ module.exports = {
     'toastify',
     'rsuite',
     'noconflict',
+    'lunz',
+    'dockerhub',
+    'kubeconfig',
+    'KUBECONFIG',
+    'kubeconfig',
+    'mkdir',
   ],
   ignorePaths: [
     '**/.idea/**',
