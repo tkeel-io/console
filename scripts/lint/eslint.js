e4190edb5865f4780b8de2f638806b5d794eef76
donniean <acmilank22@gmail.com>
2022-03-18 18:15:22 +0800

    feat: m

---
+++ scripts/lint/eslint.js
@@ -0,0 +1,14 @@
+const shell = require('shelljs');
+const path = require('path');
+const logger = require('../utils/logger');
+const { readPackageInfos } = require('../utils/packages');
+
+const packageInfos = readPackageInfos({ excludeDirectoryNames: [] });
+
+packageInfos.forEach(({ absolutePath }) => {
+  const files = path.resolve(absolutePath, '**/*.{js,ts,tsx}');
+  const configFile = path.resolve(absolutePath, '.eslintrc.js');
+  const command = `npx eslint --config ${configFile} --fix ${files}`;
+  logger.info(command);
+  shell.exec(command);
+});
