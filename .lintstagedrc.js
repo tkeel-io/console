6b33f3d062abec8fb54ac4707ed63eeb135fa2b1
donniean <acmilank22@gmail.com>
2022-03-21 18:31:15 +0800

    chore: test eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   '*.{js,ts,tsx,json,hbs,handlebars,css,scss,md,yaml,yml}': 'prettier --write',
   '*.{js,ts,tsx}': 'eslint --fix',
-  // '*.{ts,tsx}': 'tsc --noEmit',
+  '*.{ts,tsx}': 'tsc --noEmit',
   '*.{css,scss,js,ts,tsx}': 'stylelint --fix',
   '**': 'cspell --no-must-find-files',
 };
