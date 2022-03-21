ed291b96beae0569eb0a420a78761db95c897e5e
donniean <acmilank22@gmail.com>
2022-03-21 11:32:29 +0800

    chore: test eslint

--- scripts/lint/eslint.js
+++ scripts/lint/eslint.js
@@ -1,14 +1,14 @@
 const shell = require('shelljs');
-const path = require('path');
+// const path = require('path');
 const logger = require('../utils/logger');
 const { readPackageInfos } = require('../utils/packages');
 
 const packageInfos = readPackageInfos({ excludeDirectoryNames: [] });
 
 packageInfos.forEach(({ absolutePath }) => {
-  const files = path.resolve(absolutePath, '**/*.{js,ts,tsx}');
-  const configFile = path.resolve(absolutePath, '.eslintrc.js');
-  const command = `cd ${absolutePath} && npx eslint --config ${configFile} --fix ${files}`;
-  logger.info(command);
+  const files = '**/*.{js,ts,tsx}';
+  const command = `npx eslint --ignore-pattern "**/dist" "${files}"`;
+  shell.cd(absolutePath);
+  logger.info(process.cwd(), command);
   shell.exec(command);
 });
