0e7e58f46ee84580c734cf491dc35d536abb753e
donniean <acmilank22@gmail.com>
2022-03-22 19:15:21 +0800

    chore: test git commiit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{js,ts,tsx,json,hbs,handlebars,css,scss,md,yaml,yml}': 'prettier --write',
+  // '*.{js,ts,tsx,json,hbs,handlebars,css,scss,md,yaml,yml}': 'prettier --write',
   // '*.{js,ts,tsx}': 'eslint --fix',
   // '*.{ts,tsx}': 'tsc --noEmit',
   '*.{css,scss,js,ts,tsx}': 'stylelint --fix',
