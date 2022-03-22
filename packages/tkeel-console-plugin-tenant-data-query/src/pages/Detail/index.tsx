a718d221f53520e84bc39ea1258e05cdea747fb6
jackli <jackli@yunify.com>
2022-03-22 16:19:51 +0800

    feat: date-range-picker add locale

--- packages/tkeel-console-plugin-tenant-data-query/src/pages/Detail/index.tsx
+++ packages/tkeel-console-plugin-tenant-data-query/src/pages/Detail/index.tsx
@@ -39,7 +39,7 @@ export default function Detail() {
   );
   const [checkedKeys, setCheckedKeys] = useState<string[]>([]);
   const [isTelemetryDataRequested, setIsTelemetryDataRequested] =
-    useState(false);
+    useState(true);
   const [startTime, setStartTime] = useState(
     getSeconds(dayjs().subtract(1, 'hour').valueOf())
   );
@@ -231,7 +231,7 @@ export default function Detail() {
                   filename={`data-${dayjs().valueOf()}.csv`}
                 >
                   <IconButton
-                    paddingLeft="4px"
+                    paddingLeft="6px"
                     paddingRight="16px"
                     icon={<DownloadFilledIcon size={14} />}
                     isShowCircle
@@ -303,7 +303,7 @@ export default function Detail() {
             <DataResultTitle />
             <Center flex="1">
               <SearchEmpty
-                title="请选择查询条件后，点击查询按钮 "
+                title="请选择查询条件后，点击确定按钮 "
                 styles={{ image: { marginBottom: '8px', width: '104px' } }}
               />
             </Center>
