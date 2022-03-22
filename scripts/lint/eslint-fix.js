763cee623ac10f96bd59bf91a0a82f89d8885db0
donniean <acmilank22@gmail.com>
2022-03-22 12:11:21 +0800

    chore: eslint scripts

--- scripts/lint/eslint-fix.js
+++ scripts/lint/eslint-fix.js
@@ -1,17 +1,3 @@
-const path = require('path');
+const { runESLintAllFiles } = require('./exec');
 
-const { readPackageInfos } = require('../utils/packages');
-const { runESLint } = require('./exec');
-
-const packageInfos = readPackageInfos({ excludeDirectoryNames: [] });
-const isFix = true;
-
-runESLint({
-  cwd: path.resolve(__dirname, '../..'),
-  files: '**/*.{js,ts}',
-  isFix,
-});
-
-packageInfos.forEach(({ absolutePath }) =>
-  runESLint({ cwd: absolutePath, files: '**/*.{js,ts,tsx}', isFix })
-);
+runESLintAllFiles({ isFix: true });
--- scripts/lint/eslint.js
+++ scripts/lint/eslint.js
@@ -1,12 +1,3 @@
-const path = require('path');
+const { runESLintAllFiles } = require('./exec');
 
-const { readPackageInfos } = require('../utils/packages');
-const { runESLint } = require('./exec');
-
-const packageInfos = readPackageInfos({ excludeDirectoryNames: [] });
-
-runESLint({ cwd: path.resolve(__dirname, '../..'), files: '**/*.{js,ts}' });
-
-packageInfos.forEach(({ absolutePath }) =>
-  runESLint({ cwd: absolutePath, files: '**/*.{js,ts,tsx}' })
-);
+runESLintAllFiles({ isFix: false });
