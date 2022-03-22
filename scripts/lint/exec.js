2159616c40644482522c80e21435ed6c36a2a05c
donniean <acmilank22@gmail.com>
2022-03-22 12:13:34 +0800

    chore: eslint scripts (#437)
    * feat: m
    
    * feat: m
    
    * feat: delete useless files
    
    * feat:  m
    
    * feat: m
    
    * feat: add NotFound
    
    * feat: merge useGetResetPasswordKeyMutation
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: merge useUsersQuery
    
    * feat: m
    
    * feat: m
    
    * feat: merge useRolesQuery
    
    * feat: rename request
    
    * feat: password => newPassword
    
    * feat: m
    
    * feat: rename useQueryTenantMutation
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: rename
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified redirect
    
    * fix: bugs
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified login background image size
    
    * feat:  m
    
    * feat: add portal or plugin in request hooks
    
    * feat: m
    
    * feat: m
    
    * feat: get set password url from api
    
    * feat: m
    
    * feat: modified sub title
    
    * feat: adjust styles
    
    * feat: upgrade versions
    
    * feat: m
    
    * feat: upgrade chart versions
    
    * feat: m
    
    * feat: add  getRouterBasename
    
    * feat: modified src/index.tsx
    
    * feat: m
    
    * feat: rename
    
    * feat: m
    
    * refactor: delete useGlobalPluginProps
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: upgarde dependencies
    
    * feat: m
    
    * feat: m
    
    * feat:  add env
    
    * feat: m
    
    * feat: m
    
    * feat: rename request hooks
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: delete example plugin
    
    * feat: reverse config.json
    
    * feat: adjust styles
    
    * feat: rename request hooks
    
    * feat: usePluginMutation => useMutation
    
    * feat: modified tenantTitle schema
    
    * feat: add start npm script in portals
    
    * feat: m
    
    * feat: m
    
    * feat: test toast
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * refactor: toast
    
    * feat: m
    
    * fix: permision tree no empty
    
    * feat: set version to dev
    
    * feat: m
    
    * feat: m
    
    * feat: add Redirect
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: rename notRequireAuth => antiRequireAuth
    
    * feat: m
    
    * feat: rename requireNotAuth => requireNoAuth
    
    * feat: m
    
    * feat: sso
    
    * feat: upgrade dependencies
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: toast
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: add info and error icons in Alert
    
    * feat: m
    
    * feat: set toast autoClose 3000
    
    * feat: upgarde versions to 0.4.2
    
    * feat: upgarde versions to 0.4.2
    
    * feat: modified types
    
    * feat: modified getStatusInfos types
    
    * fix: bugs
    
    * feat: m
    
    * feat: m
    
    * feat: add  console-plugin-tenant-data-query chart
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: modified scripts
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * feat: m
    
    * feat: m
    
    * feat: add docker-image-tag desc
    
    * feat: add @tkeel/eslint-config-tkeel-console
    
    * feat: 0.4.3
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat: m
    
    * feat:  m
    
    * chore: test eslint
    
    * chore: test eslint
    
    * feat: eslint
    
    * chore: test eslint
    
    * chore: test eslint
    
    * feat: eslint
    
    * chore: test eslint
    
    * chore: test eslint
    
    * chore: test eslint
    
    * chore: test eslint
    
    * feat: m
    
    * chore: test eslint
    
    * feat: m
    
    * feat:  m
    
    * Create webpack.yml
    
    * Update webpack.yml
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * Update and rename webpack.yml to eslint.yml
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: rename npm scripts
    
    * chore: eslint scripts

--- scripts/lint/exec.js
+++ scripts/lint/exec.js
@@ -1,14 +1,49 @@
+const path = require('path');
+
 const shell = require('shelljs');
 
 const logger = require('../utils/logger');
+const { readPackageInfos } = require('../utils/packages');
 
 function runESLint({ cwd, files, isFix }) {
   const command = `npx eslint${isFix ? ' --fix' : ''} "${files}"`;
   logger.info(`cd ${cwd}`);
   shell.cd(cwd);
   logger.info(command);
-  shell.exec(command);
+  const result = shell.exec(command);
   logger.info();
+
+  return result;
 }
 
-module.exports = { runESLint };
+function runESLintAllFiles({ isFix } = {}) {
+  const packageInfos = readPackageInfos({ excludeDirectoryNames: [] });
+
+  const results = [];
+
+  const { stdout: rootStdout } = runESLint({
+    cwd: path.resolve(__dirname, '../..'),
+    files: '**/*.{js,ts}',
+    isFix,
+  });
+  if (rootStdout) {
+    results.push(rootStdout);
+  }
+
+  packageInfos.forEach(({ absolutePath }) => {
+    const { stdout } = runESLint({
+      cwd: absolutePath,
+      files: '**/*.{js,ts,tsx}',
+      isFix,
+    });
+    if (stdout) {
+      results.push(stdout);
+    }
+  });
+
+  if (results.length > 0) {
+    throw new Error(results.join('\n'));
+  }
+}
+
+module.exports = { runESLint, runESLintAllFiles };
