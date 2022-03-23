63040341c94dfc5ffc200eaef676a8b626a02a54
donniean <acmilank22@gmail.com>
2022-03-23 12:10:32 +0800

    feat: add --docker-image-push

--- scripts/run/build-and-docker-build.js
+++ scripts/run/build-and-docker-build.js
@@ -1,15 +1,19 @@
 #!/usr/bin/env node
 
 const build = require('../docker/build');
+const push = require('../docker/push');
 const { getSelectedCanRunPackageInfos } = require('../utils/packages');
 const { runNpmScript } = require('./commands');
 
 (async () => {
   const packageInfos = await getSelectedCanRunPackageInfos();
   packageInfos.forEach((packageInfo) => {
-    const { packageJson } = packageInfo;
+    const { packageJson, docker } = packageInfo;
     const packageName = packageJson.name;
     runNpmScript({ data: { packageName, npmScriptName: 'build' } });
     build(packageInfo);
+    if (docker?.isDockerImagePush) {
+      push(packageInfo);
+    }
   });
 })();
