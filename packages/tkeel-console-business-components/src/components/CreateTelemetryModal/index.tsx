306c69d408e2cd0b4e2ba20e6c5f6854399cc8c1
zhanghan <751818302zh@gmail.com>
2022-03-31 14:03:30 +0800

    feat: delete notes

--- packages/tkeel-console-business-components/src/components/CreateTelemetryModal/index.tsx
+++ packages/tkeel-console-business-components/src/components/CreateTelemetryModal/index.tsx
@@ -114,7 +114,6 @@ export interface FormFields {
 type Props = {
   title: ReactNode;
   isOpen: boolean;
-  // isConfirmButtonLoading: boolean;
   formFields?: FormFields;
   defaultValues?: FormValues;
   onClose: () => unknown;
@@ -137,7 +136,6 @@ function handleConfigData(
 export default function CreateTelemetryModal({
   title,
   isOpen,
-  // isConfirmButtonLoading,
   formFields,
   defaultValues,
   onClose,
@@ -205,8 +203,6 @@ export default function CreateTelemetryModal({
         RESET();
       }, 500);
     }
-
-    // }
   };
 
   const handleDataType = useCallback(
@@ -228,16 +224,6 @@ export default function CreateTelemetryModal({
     },
     [setValue, setSelectOptions]
   );
-
-  // if (defaultValues) {
-  //   // Object.entries(defaultValues.define.ext).map((item) => {
-  //   //   append({
-  //   //     label: item[0],
-  //   //     value: item[1],
-  //   //   });
-  //   // });
-  //   // defaultValues.define.ext = [];
-  // }
 
   const fieldArrayHandler = useFieldArray({
     control,
@@ -340,7 +326,6 @@ export default function CreateTelemetryModal({
     <Modal
       title={title}
       isOpen={isOpen}
-      // isConfirmButtonLoading={isConfirmButtonLoading}
       onClose={() => {
         RESET();
         onClose();
@@ -397,11 +382,9 @@ export default function CreateTelemetryModal({
             cursor="pointer"
             onClick={() => {
               append({
-                // label: `属性名称${fieldCount || ''}`,
                 label: `请修改扩展属性标题`,
                 value: '',
               });
-              // setFieldCount(fieldCount + 1);
             }}
           >
             <AddFilledIcon color="grayAlternatives.300" /> <Box>扩展配置</Box>
--- packages/tkeel-console-plugin-tenant-data-subscription/src/pages/Detail/components/MoveSubscriptionButton/index.tsx
+++ packages/tkeel-console-plugin-tenant-data-subscription/src/pages/Detail/components/MoveSubscriptionButton/index.tsx
@@ -1,15 +1,11 @@
 import { useDisclosure } from '@chakra-ui/react';
 
-// import { CreateButton } from '@tkeel/console-components';
 import { MoreActionButton } from '@tkeel/console-components';
 import { PencilFilledIcon } from '@tkeel/console-icons';
 
-// import useCreateSubscribeMutation from '@/tkeel-console-plugin-tenant-data-subscription/hooks/mutations/useCreateSubscribeMutation';
 import useMoveSubscriptionMutation from '@/tkeel-console-plugin-tenant-data-subscription/hooks/mutations/useMoveSubscriptionMutation';
-// import SetPasswordModal from '@/tkeel-console-plugin-tenant-data-subscription/pages/Index/components/SetPasswordModal';
 import useListSubscribeQuery from '@/tkeel-console-plugin-tenant-data-subscription/hooks/queries/useListSubscribeQuery';
 import MoveSubscriptionModal from '@/tkeel-console-plugin-tenant-data-subscription/pages/Detail/components/MoveSubscriptionModal';
-// import { FormValues } from '@/tkeel-console-plugin-tenant-data-subscription/pages/Index/components/BaseSubscriptionModal';
 
 type Props = {
   onSuccess: () => void;
@@ -22,48 +18,17 @@ export default function MoveSubscriptionButton({
 }: Props) {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // const { mutate } = useCreateSubscribeMutation({
-  //   onSuccess() {
-  //     onSuccess();
-  //     onClose();
-  //   },
-  // });
-
   const { data: listSubscribeData } = useListSubscribeQuery();
 
   const { mutate, isLoading } = useMoveSubscriptionMutation({
     onSuccess() {
       onSuccess();
       onClose();
-      // onSuccessModalOpen();
     },
   });
 
-  // const handleConfirm = (selectKey:string) => {
-
-  //   mutate({});
-
-  //   // if (formValues) {
-  //   //   mutate({});
-  //   // }
-  //   // return null;
-  // };
-
-  // const {
-  //   isOpen: isSuccessModalOpen,
-  //   onOpen: onSuccessModalOpen,
-  //   onClose: onSuccessModalClose,
-  // } = useDisclosure();
-
-  // const setPasswordModalData = {
-  //   tenant_id: data?.tenant_id ?? '',
-  //   user_id: data?.user_id ?? '',
-  //   username: data?.username ?? '',
-  // };
-
   return (
     <>
-      {/* <CreateButton onClick={onOpen}>创建订阅</CreateButton> */}
       <MoreActionButton
         icon={<PencilFilledIcon />}
         title="移动订阅"
