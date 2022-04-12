ecf9350e4d90cff4eb846bba4aa0248d15820f37
donniean <acmilank22@gmail.com>
2022-04-12 11:41:55 +0800

    feat: m

--- packages/tkeel-console-hooks/src/hooks/useDocuments/config.ts
+++ packages/tkeel-console-hooks/src/hooks/useDocuments/config.ts
@@ -1,5 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
 export const paths = {
-  users: '/tenant-guide/user-mgt/user_mgt',
-  roles: '/tenant-guide/user-mgt/role_mgt',
+  adminGuide: {
+    tenantIntro: '/admin-guide/tenant-mgt/tenant_intro',
+  },
+  tenantGuide: {
+    users: '/tenant-guide/user-mgt/user_mgt',
+    roles: '/tenant-guide/user-mgt/role_mgt',
+  },
 };
