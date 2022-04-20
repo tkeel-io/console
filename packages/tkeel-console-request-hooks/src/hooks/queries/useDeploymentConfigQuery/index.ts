35083fe60a28cafa445422ab76c438c4b9f884a0
donniean <acmilank22@gmail.com>
2022-04-20 12:12:45 +0800

    chore: temp

--- packages/tkeel-console-request-hooks/src/hooks/queries/useDeploymentConfigQuery/index.ts
+++ packages/tkeel-console-request-hooks/src/hooks/queries/useDeploymentConfigQuery/index.ts
@@ -15,6 +15,10 @@ export default function useDeploymentConfigQuery() {
   const result = useQuery<ApiData>({
     url: '/rudder/v1/config/deployment',
     method: 'GET',
+    // TODO: temp
+    /* extras: {
+      handleNoAuth: false,
+    }, */
   });
   const { data } = result;
   const adminHost = data?.admin_host ?? '';
