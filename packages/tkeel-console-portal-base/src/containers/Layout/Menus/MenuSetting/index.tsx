cc583826d60163f3f327fe56fd51da9614ecb635
jackli <jackli@yunify.com>
2022-04-22 16:47:19 +0800

    feat: menu setting add Tree view

--- packages/tkeel-console-portal-base/src/containers/Layout/Menus/MenuSetting/index.tsx
+++ packages/tkeel-console-portal-base/src/containers/Layout/Menus/MenuSetting/index.tsx
@@ -1,14 +1,70 @@
 import { Button, CloseButton, Flex, HStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
-import { AceEditor } from '@tkeel/console-components';
+import { AceEditor, Tree } from '@tkeel/console-components';
+import { getTreeIcon } from '@tkeel/console-utils';
 
 import { adminMenus, tenantMenus } from '@/tkeel-console-portal-base/constants';
+import { Menu } from '@/tkeel-console-portal-base/hooks/queries/useMenusQuery';
 
 type Props = {
   mockMenus: string;
   setMockMenus: (menus: string) => unknown;
   onClose: () => unknown;
 };
+
+type TreeNodeData = {
+  id: string;
+  key: string;
+  title: string;
+  children: TreeNodeData[];
+  originData: Menu;
+};
+
+function getTreeData(menus: Menu[]): TreeNodeData[] {
+  return menus.map((menu) => {
+    const { id, name, children } = menu;
+    return {
+      title: name,
+      id,
+      children: children ? getTreeData(children) : [],
+      originData: menu,
+      key: id,
+      icon: ({ expanded }: { expanded: boolean }) => getTreeIcon({ expanded }),
+    };
+  });
+}
+
+function getCheckedKeysByMenus({
+  menus,
+  totalMenus,
+}: {
+  menus: Menu[];
+  totalMenus: Menu[];
+}) {
+  const menuIds: string[] = [];
+  menus.forEach((menu) => {
+    const { id, children } = menu;
+    if (Array.isArray(children) && children.length > 0) {
+      let existMenuNum = 0;
+      children.forEach((item) => {
+        menuIds.push(item.id);
+        existMenuNum += 1;
+      });
+      const originalMenu = totalMenus.find((item) => item.id === id);
+      const { children: originalChildren } = originalMenu || {};
+      if (
+        Array.isArray(originalChildren) &&
+        existMenuNum === originalChildren.length
+      ) {
+        menuIds.push(id);
+      }
+    } else {
+      menuIds.push(id);
+    }
+  });
+  return menuIds;
+}
 
 export default function MenuSetting({
   mockMenus,
@@ -17,6 +73,36 @@ export default function MenuSetting({
 }: Props) {
   const totalMenus =
     GLOBAL_PORTAL_CONFIG.portalName === 'admin' ? adminMenus : tenantMenus;
+  const [checkedKeys, setCheckedKeys] = useState<string[]>(
+    getCheckedKeysByMenus({
+      menus: JSON.parse(mockMenus) as Menu[],
+      totalMenus,
+    })
+  );
+  const [isShowTotalMenus, setIsShowTotalMenus] = useState(false);
+  const treeData = getTreeData(totalMenus);
+
+  const getMockMenusByIds = (checkedIds: string[]) => {
+    const checkedMenus: Menu[] = [];
+    totalMenus.forEach((menu) => {
+      const { id, children } = menu;
+      if (checkedIds.includes(id)) {
+        checkedMenus.push(menu);
+      } else if (Array.isArray(children)) {
+        const checkedChildren = children.filter((item) =>
+          checkedIds.includes(item.id)
+        );
+        if (checkedChildren.length > 0) {
+          checkedMenus.push({
+            ...menu,
+            children: checkedChildren,
+          });
+        }
+      }
+    });
+    return JSON.stringify(checkedMenus, null, 2);
+  };
+
   return (
     <Flex
       position="fixed"
@@ -36,34 +122,56 @@ export default function MenuSetting({
         top="20px"
         onClick={onClose}
       />
-      <HStack flex="1" spacing="20px">
+      <HStack flex="1" paddingLeft="20px" spacing="20px">
+        <Tree
+          treeData={treeData}
+          checkable
+          defaultExpandAll
+          checkedKeys={checkedKeys}
+          selectable={false}
+          style={{ width: '200px' }}
+          onCheck={(keys) => {
+            const checkedIds = keys as string[];
+            setCheckedKeys(checkedIds);
+            setMockMenus(getMockMenusByIds(checkedIds));
+          }}
+        />
         <AceEditor
           language="json"
           value={mockMenus}
           readOnly={false}
+          style={{ flex: 1 }}
           onChange={(value) => setMockMenus(value)}
         />
-        <AceEditor
-          language="json"
-          value={JSON.stringify(totalMenus, null, 2)}
-        />
+        {isShowTotalMenus && (
+          <AceEditor
+            language="json"
+            style={{ flex: 1 }}
+            value={JSON.stringify(totalMenus, null, 2)}
+          />
+        )}
       </HStack>
-      <HStack marginTop="10px" spacing="10px">
-        <Button width="80px" onClick={onClose}>
-          关闭
+      <Flex marginTop="20px" justifyContent="space-between" alignItems="center">
+        <HStack spacing="10px">
+          <Button width="80px" onClick={onClose}>
+            关闭
+          </Button>
+          <Button
+            marginTop="10px"
+            colorScheme="brand"
+            width="80px"
+            onClick={() => {
+              sessionStorage.setItem('mockMenus', mockMenus);
+              onClose();
+            }}
+          >
+            保存
+          </Button>
+        </HStack>
+        <Button onClick={() => setIsShowTotalMenus(!isShowTotalMenus)}>
+          {isShowTotalMenus ? '隐藏' : '显示'}全部菜单
         </Button>
-        <Button
-          marginTop="10px"
-          colorScheme="brand"
-          width="80px"
-          onClick={() => {
-            sessionStorage.setItem('mockMenus', mockMenus);
-            onClose();
-          }}
-        >
-          保存
-        </Button>
-      </HStack>
+      </Flex>
     </Flex>
   );
 }
