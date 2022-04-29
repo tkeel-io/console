e24941e607d3e6a23a14257795d6e32b9ba3b459
jackli <jackli@yunify.com>
2022-04-29 17:46:22 +0800

    chore: add AppearanceConfig file

---
+++ packages/tkeel-console-plugin-admin-custom-config/src/pages/AppearanceConfig/index.tsx
@@ -0,0 +1,5 @@
+import { Flex } from '@chakra-ui/react';
+
+export default function AppearanceConfig() {
+  return <Flex>AppearanceConfig</Flex>;
+}
--- packages/tkeel-console-plugin-admin-custom-config/src/routes/index.tsx
+++ packages/tkeel-console-plugin-admin-custom-config/src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes as ReactRouterRoutes } from 'react-router-dom';
 
 import { NotFound } from '@tkeel/console-business-components';
 
+import AppearanceConfig from '../pages/AppearanceConfig';
 import Index from '../pages/Index';
 
 function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<Index />} />
+      <Route path="/appearance-config" element={<AppearanceConfig />} />
       <Route path="*" element={<NotFound />} />
     </ReactRouterRoutes>
   );
