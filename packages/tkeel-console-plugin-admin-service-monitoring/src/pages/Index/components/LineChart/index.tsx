3d0e78976a2963ed4824dde3f0abb5274b137546
donniean <acmilank22@gmail.com>
2022-05-20 12:31:35 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-service-monitoring/src/pages/Index/components/LineChart/index.tsx
+++ packages/tkeel-console-plugin-admin-service-monitoring/src/pages/Index/components/LineChart/index.tsx
@@ -1,19 +1,23 @@
 import { Box, Text } from '@chakra-ui/react';
-import { LineProps, PointTooltipProps, ResponsiveLine } from '@nivo/line';
+import { LineSvgProps, PointTooltipProps, ResponsiveLine } from '@nivo/line';
 
 function Tooltip({ point }: PointTooltipProps) {
   const { xFormatted, yFormatted } = point.data;
   return (
-    <Box>
+    <Box backgroundColor="gray.700" opacity="0.5" color="white">
       <Text>{xFormatted}</Text>
       <Text>{yFormatted}</Text>
     </Box>
   );
 }
 
-export default function LineChart(props: LineProps) {
+export default function LineChart(props: LineSvgProps) {
   return (
     <ResponsiveLine
+      yScale={{
+        type: 'linear',
+        min: 0,
+      }}
       curve="natural"
       lineWidth={1}
       colors={{ datum: 'color' }}
