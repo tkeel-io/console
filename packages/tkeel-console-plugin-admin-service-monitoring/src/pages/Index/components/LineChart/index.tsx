44c2c1a11d2724c529c6f52e95fa49026d130f15
donniean <acmilank22@gmail.com>
2022-05-20 16:14:56 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-service-monitoring/src/pages/Index/components/LineChart/index.tsx
+++ packages/tkeel-console-plugin-admin-service-monitoring/src/pages/Index/components/LineChart/index.tsx
@@ -4,9 +4,20 @@ import { LineSvgProps, PointTooltipProps, ResponsiveLine } from '@nivo/line';
 function Tooltip({ point }: PointTooltipProps) {
   const { xFormatted, yFormatted } = point.data;
   return (
-    <Box backgroundColor="gray.700" opacity="0.5" color="white">
-      <Text>{xFormatted}</Text>
-      <Text>{yFormatted}</Text>
+    <Box
+      padding="4px 12px"
+      borderRadius="4px"
+      boxShadow="0px 10px 15px rgba(113, 128, 150, 0.1), 0px 4px 6px rgba(113, 128, 150, 0.2)"
+      backgroundColor="gray.700"
+      opacity="0.9"
+      color="white"
+    >
+      <Text fontSize="12px" lineHeight="20px">
+        {xFormatted}
+      </Text>
+      <Text fontSize="12px" lineHeight="20px">
+        {yFormatted}
+      </Text>
     </Box>
   );
 }
