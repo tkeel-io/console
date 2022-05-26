6c71cbf6cf556c3ebdfd551d4e4baeeb12d81984
donniean <acmilank22@gmail.com>
2022-05-26 16:44:32 +0800

    feat: init

---
+++ packages/tkeel-console-plugin-admin-usage-statistics/config/default.js
@@ -0,0 +1,32 @@
+module.exports = {
+  portalName: 'admin',
+  publicPath: '/static/console-plugin-admin-usage-statistics/',
+  client: {
+    basePath: '/admin-usage-statistics',
+  },
+  server: {
+    port: '3019',
+  },
+  plugin: {
+    identify: {
+      plugin_id: 'console-plugin-admin-usage-statistics',
+      entries: [
+        {
+          id: 'devops',
+          name: '运维与管理',
+          icon: 'MgmtNodeTwoToneIcon',
+          children: [
+            {
+              id: 'console-plugin-admin-usage-statistics',
+              name: '用量统计',
+              path: '/admin-usage-statistics',
+              entry: '/static/console-plugin-admin-usage-statistics/',
+              portal: 0,
+            },
+          ],
+        },
+      ],
+      dependence: [],
+    },
+  },
+};
