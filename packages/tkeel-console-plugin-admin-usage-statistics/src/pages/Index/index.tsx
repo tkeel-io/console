3ff7826551a29dfc7143ebd38698e6365e8631f9
donniean <acmilank22@gmail.com>
2022-05-26 18:33:41 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Index/index.tsx
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Index/index.tsx
@@ -1,13 +1,36 @@
 import { Box, Flex } from '@chakra-ui/react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import { PageHeader, SegmentedControl } from '@tkeel/console-components';
 import { ServerNodeTwoToneIcon } from '@tkeel/console-icons';
 
+import Overview from '../Overview';
+
 const OPTIONS = [
-  { value: 'overview', label: '概览' },
-  { value: 'message', label: '消息数据统计' },
-  { value: 'usage', label: '使用数据统计' },
-  { value: 'api', label: 'API 调用统计' },
+  {
+    value: 'overview',
+    label: <Link to="/">概览</Link>,
+    path: '',
+    element: <Overview />,
+  },
+  {
+    value: 'message',
+    label: <Link to="/message">消息数据统计</Link>,
+    path: 'message',
+    element: <Overview />,
+  },
+  {
+    value: 'usage',
+    label: <Link to="/usage">使用数据统计</Link>,
+    path: 'usage',
+    element: <Overview />,
+  },
+  {
+    value: 'api',
+    label: <Link to="/api">API 调用统计</Link>,
+    path: 'api',
+    element: <Overview />,
+  },
 ];
 
 export default function Index() {
@@ -21,11 +44,15 @@ export default function Index() {
         desc="了解资源使用情况，查询实时性能指标"
       />
       <Box padding="12px 0 14px">
-        <SegmentedControl options={OPTIONS} defaultValue="overview" />
+        <SegmentedControl options={OPTIONS} defaultValue={OPTIONS[0].value} />
       </Box>
-      <Flex flexDirection="column" flex="1" overflowY="hidden">
-        1
-      </Flex>
+      <Box flex="1" overflowY="auto">
+        <Routes>
+          {OPTIONS.map(({ value, path, element }) => (
+            <Route key={value} path={path} element={element} />
+          ))}
+        </Routes>
+      </Box>
     </Flex>
   );
 }
---
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/index.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+export default function Index() {
+  return (
+    <Box flexDirection="column" flex="1" overflowY="hidden">
+      Overview
+    </Box>
+  );
+}
