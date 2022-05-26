396ffda22859a61ac568d18c265f040ff59ec334
donniean <acmilank22@gmail.com>
2022-05-26 10:06:22 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-service-monitoring/config/default.js
+++ packages/tkeel-console-plugin-admin-service-monitoring/config/default.js
@@ -14,12 +14,18 @@ module.exports = {
       plugin_id: 'console-plugin-admin-service-monitoring',
       entries: [
         {
-          id: 'console-plugin-admin-service-monitoring',
-          name: '服务监控',
-          icon: 'MonitorTwoToneIcon',
-          path: '/admin-service-monitoring',
-          entry: '/static/console-plugin-admin-service-monitoring/',
-          portal: 0,
+          id: 'devops',
+          name: '运维与管理',
+          icon: 'MgmtNodeTwoToneIcon',
+          children: [
+            {
+              id: 'console-plugin-admin-service-monitoring',
+              name: '服务监控',
+              path: '/admin-service-monitoring',
+              entry: '/static/console-plugin-admin-service-monitoring/',
+              portal: 0,
+            },
+          ],
         },
       ],
       dependence: [{ id: 'tkeel-monitor', version: tkeel.version }],
