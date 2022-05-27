b32aa6fd09743e265e223718356aa290e0871776
donniean <acmilank22@gmail.com>
2022-05-27 17:26:08 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/index.tsx
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/index.tsx
@@ -1,5 +1,11 @@
 import { Box } from '@chakra-ui/react';
 
+import ContentHeader from '@/tkeel-console-plugin-admin-usage-statistics/components/ContentHeader';
+
 export default function Overview() {
-  return <Box>Overview</Box>;
+  return (
+    <Box>
+      <ContentHeader title="概览" />
+    </Box>
+  );
 }
