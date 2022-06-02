60a4f92359c55a6e2e42f8845ebbef77d3a922a5
donniean <acmilank22@gmail.com>
2022-06-02 11:54:45 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageSubscription/MessageSubscriptionBox.tsx
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageSubscription/MessageSubscriptionBox.tsx
@@ -1,5 +1,5 @@
 import BaseBox from '@/tkeel-console-plugin-admin-usage-statistics/components/BaseBox';
 
-export default function MessageSubscription() {
+export default function MessageSubscriptionBox() {
   return <BaseBox sx={{ flex: '1' }}>1</BaseBox>;
 }
