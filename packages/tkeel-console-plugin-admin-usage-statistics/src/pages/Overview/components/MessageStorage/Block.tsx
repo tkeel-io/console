042f1f56d7b1d23933a157b28ec974cf2a34fc38
donniean <acmilank22@gmail.com>
2022-06-09 16:33:42 +0800

    feat: m

--- packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageStorage/Block.tsx
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageStorage/Block.tsx
@@ -1,33 +1,58 @@
 import type { StyleProps } from '@chakra-ui/react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Flex, Skeleton, Text } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
+
+import { numeral } from '@tkeel/console-utils';
 
 interface Props {
   label: ReactNode;
   value: ReactNode;
+  valueFormatter?: boolean | string;
+  isLoading?: boolean;
   sx?: StyleProps;
 }
 
-export default function Block({ label, value, sx }: Props) {
+const HEIGHT = '72px';
+
+export default function Block({
+  label,
+  value,
+  valueFormatter,
+  isLoading,
+  sx,
+}: Props) {
+  if (isLoading) {
+    return <Skeleton flex="1" height={HEIGHT} />;
+  }
+
   return (
-    <Box
+    <Flex
       flex="1"
-      padding="14px 20px"
+      alignItems="center"
+      height={HEIGHT}
+      padding="0 20px"
       boxShadow="4px 0px 4px rgba(239, 239, 239, 0.25)"
       backgroundColor="gray.50"
       {...sx}
     >
-      <Text
-        paddingBottom="4px"
-        fontSize="12px"
-        lineHeight="16px"
-        color="gray.500"
-      >
-        {label}
-      </Text>
-      <Text fontWeight="500" fontSize="20px" lineHeight="24px" color="gray.700">
-        {value}
-      </Text>
-    </Box>
+      <Box>
+        <Text
+          paddingBottom="4px"
+          fontSize="12px"
+          lineHeight="16px"
+          color="gray.500"
+        >
+          {label}
+        </Text>
+        <Text
+          fontWeight="500"
+          fontSize="20px"
+          lineHeight="24px"
+          color="gray.700"
+        >
+          {numeral.formatReactNode({ input: value, formatter: valueFormatter })}
+        </Text>
+      </Box>
+    </Flex>
   );
 }
--- packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageStorage/index.tsx
+++ packages/tkeel-console-plugin-admin-usage-statistics/src/pages/Overview/components/MessageStorage/index.tsx
@@ -3,12 +3,42 @@ import { Box, Flex, HStack, Text } from '@chakra-ui/react';
 import { Tips } from '@tkeel/console-components';
 
 import BaseBox from '@/tkeel-console-plugin-admin-usage-statistics/components/BaseBox';
+import usePrometheusTKMeterBatchQuery from '@/tkeel-console-plugin-admin-usage-statistics/hooks/queries/usePrometheusTKMeterBatchQuery';
+import { findValue } from '@/tkeel-console-plugin-admin-usage-statistics/utils/query';
 
 import ModuleHeader from '../ModuleHeader';
 import Block from './Block';
 import MessageStorageItem from './MessageStorageItem';
 
+const METERS = [
+  'msg_storage_days',
+  'core_msg_days',
+  'msg_storage_space_unused_bytes',
+  'msg_storage_space_usage_percentage',
+];
+
 export default function MessageStorage() {
+  const { isLoading, items } = usePrometheusTKMeterBatchQuery({
+    params: { meters: METERS },
+  });
+
+  const msgStorageDays = findValue({
+    data: items,
+    query: METERS[0],
+  });
+  const coreMSgDays = findValue({
+    data: items,
+    query: METERS[1],
+  });
+  const msgStorageSpaceUnusedBytes = findValue({
+    data: items,
+    query: METERS[2],
+  });
+  const msgStorageSpaceUsagePercentage = findValue({
+    data: items,
+    query: METERS[3],
+  });
+
   return (
     <Box width="100%">
       <ModuleHeader
@@ -20,8 +50,16 @@ export default function MessageStorage() {
         <HStack spacing="44px">
           <MessageStorageItem title="历史存储" sx={{ paddingBottom: '28px' }}>
             <HStack spacing="12px">
-              <Block label="历史消息存储 (天)" value="17" />
-              <Block label="日存储量 (条)" value="18" />
+              <Block
+                label="历史消息存储 (天)"
+                value={msgStorageDays}
+                isLoading={isLoading}
+              />
+              <Block
+                label="日存储量 (条)"
+                value={coreMSgDays}
+                isLoading={isLoading}
+              />
             </HStack>
           </MessageStorageItem>
           <MessageStorageItem
@@ -29,7 +67,12 @@ export default function MessageStorage() {
             sx={{ paddingBottom: '28px' }}
           >
             <HStack spacing="12px">
-              <Block label="时序可用存储空间" value="19T" />
+              <Block
+                label="时序可用存储空间"
+                value={msgStorageSpaceUnusedBytes}
+                valueFormatter="0 b"
+                isLoading={isLoading}
+              />
               <Block
                 label={
                   <Flex alignItems="center">
@@ -37,7 +80,9 @@ export default function MessageStorage() {
                     <Tips label="请注意使用占比，超过 80% 后需要扩容" />
                   </Flex>
                 }
-                value="20%"
+                value={msgStorageSpaceUsagePercentage / 100}
+                valueFormatter="0%"
+                isLoading={isLoading}
               />
             </HStack>
           </MessageStorageItem>
