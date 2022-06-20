c4e64ce7912c4340d96d6971676023b7a606bea6
jackli <jackli@yunify.com>
2022-06-20 09:58:29 +0800

    chore: revert .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,ts,tsx,json,hbs,handlebars,css,scss,md,yaml,yml}': 'prettier --write',
-  // '*.{js,ts,tsx}': 'eslint --fix',
+  '*.{js,ts,tsx}': 'eslint --fix',
   // '*.{ts,tsx}': 'tsc --noEmit',
   // '*.{css,scss,js,ts,tsx}': 'stylelint --fix',
   '**': 'cspell --no-must-find-files',
