266d73dee2c4659606e30ecb76f7b40c392260e7
jackli <jackli@yunify.com>
2022-06-22 11:02:42 +0800

    feat: add appearanceConfig doc

--- packages/tkeel-console-hooks/src/hooks/useDocuments/config.ts
+++ packages/tkeel-console-hooks/src/hooks/useDocuments/config.ts
@@ -5,6 +5,9 @@ export const paths = {
       '/admin-guide/tenant-mgt/tenant_create#配置认证协议',
     plugins: '/admin-guide/plugin-mgt/plugin_intro',
     customConfig: '/admin-guide/platform-config/config_customize',
+    themeColorConfig:
+      '/admin-guide/platform-config/config_customize#主题色配置',
+    appearanceConfig: '/admin-guide/platform-config/config_customize#外观配置',
     serviceMonitoring: '/admin-guide/maintenance-mgt/service_monitor',
   },
   tenantGuide: {
