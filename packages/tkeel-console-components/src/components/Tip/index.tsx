67c821affd363114a14579487639e6315e29458d
jackli <jackli@yunify.com>
2022-06-24 17:07:48 +0800

    feat: component ConfigureNotificationModal add Tip

--- packages/tkeel-console-components/src/components/Tip/index.tsx
+++ packages/tkeel-console-components/src/components/Tip/index.tsx
@@ -6,15 +6,16 @@ import { TipIcon } from '@tkeel/console-icons';
 type Props = {
   icon?: ReactNode;
   title: string;
+  sx?: StyleProps;
   styles?: {
-    wrapper?: StyleProps;
+    root?: StyleProps;
     text?: StyleProps;
   };
 };
 
-export default function Tip({ icon, title, styles }: Props) {
+export default function Tip({ icon, title, sx, styles }: Props) {
   return (
-    <Flex alignItems="center" {...styles?.wrapper}>
+    <Flex alignItems="center" {...styles?.root} {...sx}>
       {icon || <TipIcon size={14} color="gray.500" />}
       <Text ml="8px" color="gray.500" fontSize="12px" {...styles?.text}>
         {title}
