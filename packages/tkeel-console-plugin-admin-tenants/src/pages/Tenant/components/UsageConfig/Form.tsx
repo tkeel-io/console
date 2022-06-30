b810c18b4eaf7499d0ca61b5bfafd796fe65c494
donniean <acmilank22@gmail.com>
2022-06-30 11:20:34 +0800

    fix: required not tips

--- packages/tkeel-console-plugin-admin-tenants/src/pages/Tenant/components/UsageConfig/Form.tsx
+++ packages/tkeel-console-plugin-admin-tenants/src/pages/Tenant/components/UsageConfig/Form.tsx
@@ -80,7 +80,11 @@ export default function Form({ schema, data, refetchData }: Props) {
         // eslint-disable-next-line unicorn/no-array-reduce
         const formErrors = (ajvErrors ?? []).reduce<Record<string, FieldError>>(
           (prev, ajvError) => {
-            const { instancePath, keyword, message } = ajvError;
+            const { keyword, message, params } = ajvError;
+            let { instancePath } = ajvError;
+            if (keyword === 'required') {
+              instancePath = `/${params.missingProperty as string}`;
+            }
             const key = instancePath.slice(1);
 
             return {
