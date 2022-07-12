9ce918781c54ef22a29d79f7f45e1171246f5d6a
jackli <jackli@yunify.com>
2022-07-12 16:47:45 +0800

    chore: modify useNotificationsQuery

--- packages/tkeel-console-portal-base/src/hooks/queries/useNotificationsQuery/index.ts
+++ packages/tkeel-console-portal-base/src/hooks/queries/useNotificationsQuery/index.ts
@@ -4,8 +4,7 @@ const url = '/rudder/v1/notifications';
 const method = 'GET';
 
 enum NotificationActionTypes {
-  InternalJump = 'internal-jump', // 或者 number，比如：0, 1, 2
-  // ExternalJump = 'external-jump', // 举例 type，目前不需要
+  InternalJump = 'internal-jump',
 }
 
 export interface Notification {
@@ -17,17 +16,14 @@ export interface Notification {
     action: {
       type: NotificationActionTypes;
       value?: string;
-      // 不同 type 的额外扩展信息，比如 type 为 internal-jump 和 external-jump 时，可以添加 is_open_in_new_window 来标识是否用新窗口打开链接
       extras?: {
         is_open_in_new_window?: boolean;
-        // 其他信息。。。
       };
     };
   };
   entry?: {
     id: string;
     name: string;
-    // count: number;
   };
 }
 
