cecbf22c61dd9a283335a3bfad2701cc4081f59a
miaqiang <qiangminmail@gmail.com>
2022-07-15 09:29:27 +0800

    fix: some bugs (#713)
    * fix: tkeel-599
    
    * fix: tkell-699
    
    * fix: telemetry can't MultiSelect
    
    Co-authored-by: miaqiang <qiangmin@yunify.com>

--- packages/tkeel-console-plugin-tenant-device-templates/src/pages/Detail/components/AttributeTable/index.tsx
+++ packages/tkeel-console-plugin-tenant-device-templates/src/pages/Detail/components/AttributeTable/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { CellProps, Column } from 'react-table';
 
 import {
@@ -88,8 +88,8 @@ function AttributeTable({ id, title }: { id: string; title: string }) {
     {
       Header: '操作',
       width: 80,
-      Cell: ({ row }: CellProps<AttributeItem>) =>
-        useMemo(() => {
+      Cell: useCallback(
+        ({ row }: CellProps<AttributeItem>) => {
           const { original } = row;
 
           return (
@@ -110,7 +110,9 @@ function AttributeTable({ id, title }: { id: string; title: string }) {
               ]}
             />
           );
-        }, [row]),
+        },
+        [id, refetch]
+      ),
     },
   ];
 
