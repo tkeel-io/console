f81368f71fced971dd30c5ac24eaba0b7a79db7e
donniean <acmilank22@gmail.com>
2022-07-21 10:14:40 +0800

    feat: delete useless comments

--- webpack/webpack.config.portals.js
+++ webpack/webpack.config.portals.js
@@ -15,21 +15,6 @@ const webpackConfigPath = path.resolve(paths.root.webpack, 'webpack.config.js');
 // eslint-disable-next-line import/no-dynamic-require
 const webpackConfig = require(webpackConfigPath)();
 
-/* function createConfigJsonFile() {
-  const data = {
-    code: 'io.tkeel.SUCCESS',
-    msg: '',
-    data: {
-      client: config.client,
-    },
-  };
-  const filePath = path.resolve(paths.cwd.self, 'public/config/config.json');
-  fs.outputJSONSync(filePath, data);
-  shell.exec(`prettier --write ${filePath}`);
-}
-
-createConfigJsonFile(); */
-
 const webpackConfigPortals = {
   plugins: [
     new CopyPlugin({
